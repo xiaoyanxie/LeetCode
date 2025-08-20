dfdf1ef1872b40144dc3facfed7e92686234d0db
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2025-08-20 05:54:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19 MB (19.81%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,22 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashMap = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map ={}
         for i, num in enumerate(nums):
-            complement = target - num
-            if complement in hashMap:
-                return [hashMap[complement], i]
+            remain = target - num
+            if remain in map:
+                return [map[remain], i]
             else:
-                hashMap[num] = i
-        #nested for loop to check
-        # length = len(nums)
-        # for num in range(length):
-        #     for i in range(num + 1, length):
-        #         if(nums[num] + nums[i]) == target:
-        #             return [num,i]
-                
-        # return False
+                map[num] = i
+        
+        return []
+

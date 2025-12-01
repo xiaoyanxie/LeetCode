e5a3fb8eb63a13febbee3f0bf368326a493593e2
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2025-12-01 02:05:24 +0000

    [LeetCode Sync] Runtime - 11 ms (95.01%), Memory - 18.1 MB (9.72%)

--- leetcode/0003-longest-substring-without-repeating-characters/solution.py
+++ leetcode/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,40 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #check the current one in visited ones or not
-        #not in, count +1
-        # in, count vs longest and update longest, clear set, move right
-        
-        # longest = 0
-        # n = len(s)
-        # if n == 1 and s == " ":
-        #     return 1
-        # if n == 0:
-        #     return 0
-        # for i in range(n):
-        #     container = set()
-        #     count = 0
-        #     for ele in range(i, n):
-        #         if s[ele] not in container:
-        #             container.add(s[ele])
-        #             count = count + 1
-        #         else:
-        #             longest = max(longest, count)
-        #             break
-        #     else:
-        #         longest = max(longest, count)
-        # return longest
-        left = 0
-        n = len(s)
-        seen = set()
-        longest = 0
-        for right, val in enumerate(s):   
-            while val in seen:
-                seen.remove(s[left])
-                left += 1
+        seen = {}
 
-            seen.add(val)
-            longest = max(longest, right - left + 1)
-        return longest
+        l = 0
+        maxLen = 0
 
+        for r, c in enumerate(s):
+            if c in seen and seen[c] >= l:
+                l = seen[c] + 1
+            seen[c] = r
+            maxLen = max(maxLen, r - l + 1)
 
+        return maxLen
 

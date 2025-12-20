3feb249b99a62b7e70698588619cef703c17669f
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2025-12-20 21:53:49 +0000

    [LeetCode Sync] Runtime - 3 ms (76.70%), Memory - 19.3 MB (86.51%)

--- leetcode/0252-meeting-rooms/solution.py
+++ leetcode/0252-meeting-rooms/solution.py
@@ -1,20 +1,6 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        # used = set()
-        # for i in intervals:
-        #     start = i[0]
-        #     end = i[1]
-        #     for j in range(start, end):
-        #         if j in used:
-        #             return False
-        #         else:
-        #             used.add(j)
-        
-        # return True
 
-# Get each interval by itrating the list of list, then put the number in this interval into the set if the set doesn't contain, return false if the set contains the number.
-#Time complexity: #interval * # element in one interval: O(N * K).
-#Space complexity: O(N*K)
         intervals.sort()
         for i in range(1, len(intervals)):
             if intervals[i][0] >= intervals[i-1][1]:
@@ -23,3 +9,11 @@ class Solution:
                 return False
         
         return True
+
+# Get sorted list of intervals, which means the intervals are sorted by the first ele then by the second ele. Then compare the start time of current interval with the ending time of the last interval to find out if there is overlap
+
+#Time complexity: sort: O(nlogn), compare O(n) where n is the number of intervals. overall runtime: O(nlogn)
+
+#space complexity: sort is in place O(n), for python sort algo is a combination of Merge sort and insertion sort, compare takes extra O(1) space. ovrall space complexity is O(n)
+
+

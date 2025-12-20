8364fd4eabb240cea8baa8771c9f14d843aac2e3
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2025-12-20 01:33:41 +0000

    [LeetCode Sync] Runtime - 8 ms (33.25%), Memory - 20.5 MB (98.07%)

--- leetcode/0347-top-k-frequent-elements/solution.py
+++ leetcode/0347-top-k-frequent-elements/solution.py
@@ -1,5 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        return [num for num, _ in count.most_common(k)]
+        #get frequency of each number
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
         
+        #sort by frequency
+        num_list = list(freq.keys())
+        num_list.sort(key=lambda x: -freq[x])
+        #default sort is acsending
+        return num_list[:k]
+        
+
+
+    #First get the frequency of each number
+    #Second get the sort 

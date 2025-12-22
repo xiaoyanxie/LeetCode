62cc9af853e1174d648d2b0de96ca3c8b6be1e1d
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2025-12-22 06:12:44 +0000

    [LeetCode Sync] Runtime - 3 ms (50.25%), Memory - 18.6 MB (59.10%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        # for each item in the list, if it is sum pair was seen, return sum pair, otherwise store the number and index in the list
-        for i, n in enumerate(nums):
-            m = target - n
-            if m in seen:
-                return [seen[m], i]
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [i, seen[target-num]]
             else:
-                seen[n] = i
-        return [-1, -1]
-
+                seen[num]=i

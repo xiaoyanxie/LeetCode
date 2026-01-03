a12c4b6818f58454bc2bfe9666f05edc379d1623
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-03 00:08:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (86.02%)

--- leetcode/0111-minimum-depth-of-binary-tree/solution.py
+++ leetcode/0111-minimum-depth-of-binary-tree/solution.py
@@ -6,15 +6,38 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if root == None:
-            return 0
-        if root.left == None and root.right == None:
-            return 1
-        if root.left == None and root.right != None:
-            return self.minDepth(root.right) + 1
-        elif root.right == None and root.left != None:
-            return self.minDepth(root.left) + 1
-        elif root.left != None and root.right != None:
-            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
-        
+        #use dfs to find the shortest path
+        # if not root:
+        #     return 0
 
+        # mindep = float('inf')
+        # stack=[(root,1)]     
+
+        # while stack:
+        #     node, dep = stack.pop()
+        #     if not node.left and not node.right:
+        #         mindep = min(mindep,dep)
+        #     if node.right:
+        #         stack.append((node.right,dep + 1))
+        #     if node.left:
+        #         stack.append((node.left,dep + 1))
+            
+        # return mindep  
+
+        #use bfs to find the path length to the first childrenless node
+        if not root:
+            return 0
+        queue = deque()
+        queue.append((root,1))  
+        
+        while queue:
+            node, dep = queue.popleft()
+            if not node.left and not node.right:
+                return dep
+            if node.right:
+                queue.append((node.right,dep + 1))
+            if node.left:
+                queue.append((node.left,dep + 1))
+                        
+
+# It depends on the topology of the tree. If the tree is very deep, use BFS to save sapce. If the tree is wide and shallow, use DFS to save space. If the tree is perfect binary tree(every level is full), height = logN, width = N/2, use DFS to save space. If the tree is slanted, BFS is a better choice which uses O(1) extra space instead of O(N) for DFS.

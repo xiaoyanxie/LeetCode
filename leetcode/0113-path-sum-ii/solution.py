e6d5ca517b9eaa7757f6d54199a0867b2f5720e6
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-04 07:38:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (94.96%)

--- leetcode/0113-path-sum-ii/solution.py
+++ leetcode/0113-path-sum-ii/solution.py
@@ -6,21 +6,43 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        # if not root:
+        #     return []    
+    #     res = []
+    #     path = [root.val]
+    #     stack = [(root, root.val, path)]
+
+    #     while stack:
+    #         node, psum, path = stack.pop()
+    #         if not node.left and not node.right:
+    #             if psum == targetSum:
+    #                 res.append(path)
+    #         if node.left:
+    #             stack.append((node.left, psum + node.left.val, path + [node.left.val]))
+    #         if node.right:
+    #             stack.append((node.right, psum + node.right.val, path + [node.right.val]))
+
+    #     return res
+
+    # #runtime: N * linkedlist O(N)=O(N**2)
+    # #space: O（N **2)for linked list shape, O(NlogN) for balanced tree
+
+        def dfs(node, path, remaining):
+            if not node:
+                return
+            path.append(node.val)
+            if not node.left and not node.right and remaining == 0:
+                res.append(list(path))
+            if node.left:
+                dfs(node.left, path, remaining - node.left.val)
+            if node.right:
+                dfs(node.right, path, remaining - node.right.val)
+            
+            path.pop()
+
         if not root:
             return []
-
         res = []
-        path = [root.val]
-        stack = [(root, root.val, path)]
-
-        while stack:
-            node, psum, path = stack.pop()
-            if not node.left and not node.right:
-                if psum == targetSum:
-                    res.append(path)
-            if node.left:
-                stack.append((node.left, psum + node.left.val, path + [node.left.val]))
-            if node.right:
-                stack.append((node.right, psum + node.right.val, path + [node.right.val]))
-
+        path = []
+        dfs(root, path, targetSum - root.val)
         return res

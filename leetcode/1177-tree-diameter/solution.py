bd6261af9b28234dd3e6f237b815643c744ca04f
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-06 22:33:01 +0000

    [LeetCode Sync] Runtime - 15 ms (83.93%), Memory - 24.2 MB (10.12%)

--- leetcode/1177-tree-diameter/solution.py
+++ leetcode/1177-tree-diameter/solution.py
@@ -1,52 +1,104 @@
 class Solution:
     def treeDiameter(self, edges: List[List[int]]) -> int:
-        # edges.sort()
-        if not edges or not edges[0]:
-            return 0
-
-        #O(E)
-        hmap = {}
-        for edge in edges:
-            if edge[0] not in hmap.keys():
-                hmap[edge[0]] = list()
-            if edge[1] not in hmap.keys():
-                hmap[edge[1]] = list()
-            hmap[edge[0]].append(edge[1])
-            hmap[edge[1]].append(edge[0])
-      
         visited = set()
-        self.maxD = 0
         def dfs(graph, root) -> int:
-            visited.add(root)
             if not graph[root]:
                 return 0
-            heights = []
+            visited.add(root)
+            # height = []
+            top1 = 0
+            top2 = 0
             for adj in graph[root]:
                 if adj in visited:
                     continue
-                height = 1 + dfs(graph, adj)
-                heights.append(height)
+                hei = 1 + dfs(graph, adj)
+                if hei > top1:
+                    top2 = top1
+                    top1 = hei
+                elif hei > top2:
+                    top2 = hei
+                # height.append(hei)
             
-            if not heights:
-                return 0
+            # if not height:
+            #     return 0
+            self.maxLen = max(self.maxLen, top1+top2)
+            # height.sort()
+            # if len(height) == 1:
+            #     self.maxLen = max(self.maxLen, height[-1])
+            # elif len(height) > 1:
+            #     self.maxLen = max(self.maxLen, height[-1] + height[-2])
             
-            heights.sort()
-            if len(heights) == 1:
-                self.maxD = max(self.maxD, heights[-1])
-            else:
-                self.maxD = max(self.maxD, heights[-1] + heights[-2])
-            return max(heights)
+            return top1
 
-        dfs(hmap, edges[0][0])
-        return self.maxD
-        # top1 =0
-        # top2 = 0
-        # for value in hmap.values():
-        #     length = len(value)
-        #     if length > top1:
-        #         top2 = top1
-        #         top1 = value
-        #     if length > top2:
-        #         top2 = value
-        # return top1+top2
+        if not edges:
+            return 0
+        graph = {}
+        for edge in edges:
+            if edge[0] not in graph.keys():
+                graph[edge[0]] = list()
+            if edge[1] not in graph.keys():
+                graph[edge[1]] = list()
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+        
+        self.maxLen = 0
+        
+        dfs(graph, edges[0][0])
+        return self.maxLen
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # # edges.sort()
+        # if not edges or not edges[0]:
+        #     return 0
+
+        # #O(E)
+        # hmap = {}
+        # for edge in edges:
+        #     if edge[0] not in hmap.keys():
+        #         hmap[edge[0]] = list()
+        #     if edge[1] not in hmap.keys():
+        #         hmap[edge[1]] = list()
+        #     hmap[edge[0]].append(edge[1])
+        #     hmap[edge[1]].append(edge[0])
+      
+        # visited = set()
+        # def dfs(graph, root) -> int:
+        #     visited.add(root)
+        #     if not graph[root]:
+        #         return 0
+        #     heights = []
+        #     for adj in graph[root]:
+        #         if adj in visited:
+        #             continue
+        #         height = 1 + dfs(graph, adj)
+        #         heights.append(height)
+            
+        #     if not heights:
+        #         return 0
+            
+        #     heights.sort()
+        #     if len(heights) == 1:
+        #         self.maxD = max(self.maxD, heights[-1])
+        #     else:
+        #         self.maxD = max(self.maxD, heights[-1] + heights[-2])
+        #     return max(heights)
+
+        # self.maxD = 0
+        # dfs(hmap, edges[0][0])
+        # return self.maxD
+        
 

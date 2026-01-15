92232012529b1c732372186b00c37638df654007
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-15 04:38:52 +0000

    [LeetCode Sync] Runtime - 41 ms (63.06%), Memory - 20.7 MB (5.16%)

---
+++ leetcode/0572-subtree-of-another-tree/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSame(root, subRoot):
+            if not root and not subRoot:
+                return True
+            if not root or not subRoot:
+                return False
+            
+            if root.val != subRoot.val:
+                return False
+                
+            # if root.val == subRoot.val:
+            return isSame(root.left, subRoot.left) and isSame(root.right, subRoot.right)
+
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        
+        if isSame(root, subRoot):
+            return True
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+        

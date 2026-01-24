59897ac7b6c78f9a6691066b5c420f3ece0231c5
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-24 15:11:27 +0000

    [LeetCode Sync] Runtime - 3 ms (86.92%), Memory - 21.2 MB (12.18%)

--- leetcode/0207-course-schedule/solution.py
+++ leetcode/0207-course-schedule/solution.py
@@ -2,28 +2,30 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:           
         if not prerequisites:
             return True
-
+        
         adj = [[] for _ in range(numCourses)]
-        for a, b in prerequisites:
+        for a,b in prerequisites:
             adj[b].append(a)
+        
+        state = [0] * numCourses
 
         def dfs(prereq):
+
             if state[prereq] == 2:
                 return True
             if state[prereq] == 1:
                 return False
-
+            
             state[prereq] = 1
-            for c in adj[prereq]:
-                if not dfs(c):
+            for course in adj[prereq]:
+                if not dfs(course):
                     return False
-            state[prereq] = 2
+                state[course] = 2
             return True
 
-        state = [0] * numCourses
         for i in range(numCourses):
             if state[i] == 0:
                 if not dfs(i):
                     return False
-        
+            state[i] = 2
         return True

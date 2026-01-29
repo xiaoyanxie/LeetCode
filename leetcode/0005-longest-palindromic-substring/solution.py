2410f0ac434f3be9f79e7da6c11c7afba37bdc03
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-29 08:13:57 +0000

    [LeetCode Sync] Runtime - 360 ms (41.18%), Memory - 19.5 MB (25.66%)

--- leetcode/0005-longest-palindromic-substring/solution.py
+++ leetcode/0005-longest-palindromic-substring/solution.py
@@ -1,23 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def buildPalindrome(l: int, r: int):
-            while 0 <= l and r < len(s) and s[l] == s[r]:
-                l = l - 1
-                r = r + 1
-            return l + 1, r - 1
+        # abaabba
+
+        def expand(i, j):
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            return (i + 1, j)
+
+        best = ""
+        for i in range(len(s)):
+            print(f"search from s[{i}] = {s[i]}")
+            start, end = expand(i, i)
+            print(f"found {s[start:end]}")
+            if end - start > len(best):
+                best = s[start:end]
+            if i + 1 == len(s):
+                continue
+
+            print(f"search from s[{i}] = {s[i]} and s[{i + 1}] = {s[i + 1]}")
+            start1, end1 = expand(i, i + 1)
+            print(f"found {s[start1:end1]}")
+            if end1 - start1 > len(best):
+                best = s[start1:end1]
         
-        start, maxLen = 0, 0
-        for i in range(0, len(s)):
-            l1, r1 = buildPalindrome(i, i)
-            p1 = r1 - l1 + 1
-            if p1 > maxLen:
-                maxLen = p1
-                start = l1
-            
-            l2, r2 = buildPalindrome(i, i + 1)
-            p2 = r2 - l2 + 1
-            if p2 > maxLen:
-                maxLen = p2 
-                start = l2
-        
-        return s[start: start + maxLen]
+        return best

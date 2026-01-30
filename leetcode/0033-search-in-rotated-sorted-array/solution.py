942cdf7b8a1441d0ec13618b2c47b0e8821a3cc0
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-30 21:58:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (18.14%)

--- leetcode/0033-search-in-rotated-sorted-array/solution.py
+++ leetcode/0033-search-in-rotated-sorted-array/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
-        
         def get_side(n):
             if nums[0] > n <= nums[-1]:
                 return 'right'
             else:
                 return 'left'
-
+            
+        l = 0
+        r = len(nums) - 1
         while l <= r:
             mid = l + (r - l) // 2
-            
             if nums[mid] == target:
                 return mid
-            elif get_side(nums[mid]) == get_side(target):
+            if get_side(nums[mid]) == get_side(target):
                 if nums[mid] < target:
                     l = mid + 1
                 else:

3e8be27f1ee6e0c34ee9c2d8f4d3d55ec39041fd
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-01-30 18:26:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (13.78%)

--- leetcode/0078-subsets/solution.py
+++ leetcode/0078-subsets/solution.py
@@ -1,18 +1,27 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        # res = []
 
-        def dfs(i, path):
-            if i == len(nums):
-                res.append(path.copy()) 
-                return 
+        # def dfs(i, path):
+        #     if i == len(nums):
+        #         res.append(path.copy()) 
+        #         return 
 
-            path.append(nums[i])
-            dfs(i + 1, path)
-            path.pop()
-            dfs(i + 1, path)
+        #     path.append(nums[i])
+        #     dfs(i + 1, path)
+        #     path.pop()
+        #     dfs(i + 1, path)
             
-        
-        dfs(0, [])
+        # dfs(0, [])
+        # return res
 
+        res = []
+        def dfs(start, path):
+            res.append(path.copy())
+
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                dfs(i+1, path)
+                path.pop()
+        dfs(0, [])
         return res

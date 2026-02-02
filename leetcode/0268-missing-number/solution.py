f8e7f2627a4d7502f43e42540ea47680e12da73e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-02 01:20:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (20.76%)

--- leetcode/0268-missing-number/solution.py
+++ leetcode/0268-missing-number/solution.py
@@ -1,15 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #put all numbers in set
-        #check size of nums
-        #check each number
-        container = set()
-        for number in nums:
-            container.add(number)
-        length = len(nums)
-
-        for i in range(length + 1):
-            if i not in container:
-                return i
-
-        return -1
+        actual_sum = sum(nums)
+        n = len(nums)
+        expected_sum = int((n * (n + 1)) / 2)
+        return expected_sum - actual_sum

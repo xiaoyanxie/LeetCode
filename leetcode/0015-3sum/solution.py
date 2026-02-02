10ab8a2048e9776ef0d2719fdb6884100d30418b
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-02 03:48:05 +0000

    [LeetCode Sync] Runtime - 499 ms (80.66%), Memory - 22.3 MB (25.33%)

--- leetcode/0015-3sum/solution.py
+++ leetcode/0015-3sum/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = []
-        for k in range (0, len(nums)):
-            if k >0 and nums[k] == nums[k-1]:
-                continue
+        ret = []
+        n = len(nums)
 
-            i = k + 1
-            j = len(nums) - 1
-            comp = -nums[k]
-            while i < j:
-                s = nums[i] + nums[j]
-                if s == comp:
-                    res.append([nums[i], nums[j], nums[k]])
-                    while i < j and nums[i] == nums[i+1]:
-                        i+=1
-                    while i < j and nums[j] == nums[j-1]:
-                        j-=1
-                    i+=1
-                    j-=1
-                elif s > comp:
-                    j-=1
-                elif s < comp:
-                    i+=1
-        return res
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            l, r = i + 1, n - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    ret.append([ nums[i], nums[l], nums[r] ])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+        return ret

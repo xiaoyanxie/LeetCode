584563944d3482d4a2b39f4b51654290ef4afe17
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-03 07:40:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (14.55%)

--- leetcode/0020-valid-parentheses/solution.py
+++ leetcode/0020-valid-parentheses/solution.py
@@ -1,32 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        res = []
-       
-        open = ["(","[","{"]
-        close = [")","]","}"]
+        mp = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        stack = []
         for p in s:
-            if p in open:
-                res.append(p)
-            else:
-                if (not res) or (open.index(res[-1]) != close.index(p)):
-                    return False
-                else:
-                    res.pop()
-        
-        return not res
-        # for p in s:
-        #     if p in "[{(":
-        #         res.append(p)
-        #     elif p in "]})":
-        #         if not res:
-        #             return False
-        #         if p == "]" and res[-1] =="[":
-        #             res.pop()
-        #         elif p == ")" and res[-1] =="(":
-        #             res.pop()
-        #         elif p == "}" and res[-1] =="{":
-        #             res.pop()
-        #         else:
-        #             return False
-                
-        # return len(res)==0
+            if p in mp:
+                stack.append(p)
+            elif len(stack) == 0:
+                return False
+            elif mp[stack.pop()] != p:
+                return False
+        return len(stack) == 0

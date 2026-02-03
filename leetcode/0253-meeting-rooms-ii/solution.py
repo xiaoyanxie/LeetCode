516b537e0be5a9110332b3c9b9bd8d6b910c415e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-03 08:12:17 +0000

    [LeetCode Sync] Runtime - 8 ms (14.60%), Memory - 21.5 MB (39.39%)

--- leetcode/0253-meeting-rooms-ii/solution.py
+++ leetcode/0253-meeting-rooms-ii/solution.py
@@ -1,13 +1,13 @@
+import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        used = []
-        heapq.heappush(used,intervals[0][1])
-        for i in range(1, len(intervals)):
-            if (intervals[i][0]) >= used[0]:
-                old = heapq.heappop(used)
-                heapq.heappush(used,intervals[i][1])
-            else:
-                heapq.heappush(used,    intervals[i][1])
-        return len(used)
         
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
+        for i in range(1, len(intervals)):
+            if intervals[i][0] >= heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, intervals[i][1])
+
+        return len(heap)

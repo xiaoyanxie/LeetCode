0fcac93359074484d988e2d1d788e91e856ca2fc
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-04 07:09:09 +0000

    [LeetCode Sync] Runtime - 242 ms (62.54%), Memory - 21.4 MB (66.38%)

--- leetcode/0200-number-of-islands/solution.py
+++ leetcode/0200-number-of-islands/solution.py
@@ -1,27 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(grid, i, j):
-            if grid[i][j] == "0":
-                return
-            grid[i][j] = "0"
-            for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
-                nx, ny = i + dx, j + dy
-                if 0<= nx < len(grid) and 0 <= ny < len(grid[0]):
-                    dfs(grid, nx, ny)
-            
-            return
-        
-        count = 0
+        # Time: O(M * N)
+        # Space: O(1)
+    
+        # visited = set()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
         rows = len(grid)
         cols = len(grid[0])
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "0":
-                    continue
-                dfs(grid, i, j)
-                count += 1
-        
-                
-        
-        return count
-        
+
+        def dfs(i, j):
+            stack = [(i, j)]
+            grid[i][j] = '0'
+            while stack:
+                i, j = stack.pop()
+                # neighbors
+                for d in directions:
+                    k, l = i + d[0], j + d[1]
+                    if 0 <= k < rows and 0 <= l < cols and grid[k][l] == '1':
+                        stack.append((k, l))
+                        grid[k][l] = '0'
+
+        islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    islands += 1
+
+        return islands

cde2f346bbacfe200133d32a0813201c4810946e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-04 21:32:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (20.03%)

--- leetcode/0054-spiral-matrix/solution.py
+++ leetcode/0054-spiral-matrix/solution.py
@@ -1,38 +1,48 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        row = len(matrix)
-        col = len(matrix[0])
-        l = 0
-        r = col
-        top = 0
-        bott = row
-        res = []
-        length = row * col
-       
-        while l < r and top < bott:
-            for i in range(l, r):
-                res.append(matrix[top][i])
-            #update top bound
-            top += 1
-            for i in range(top, bott):
-                res.append(matrix[i][r - 1])
-            #update right bound
-            r -= 1
-            if not(l < r and top < bott):
-                return res
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        #.             right,  down,    left.    up
+        currentDirection = 0
+        rowsLo = 0
+        rowsHi = len(matrix)
+        colsLo = 0
+        colsHi = len(matrix[0])
 
-            for i in range(r - 1, l - 1, -1):
-                res.append(matrix[bott - 1][i])
-            #update bott bound
-            bott -= 1
+        def nextDirection():
+            nonlocal currentDirection, rowsLo, rowsHi, colsLo, colsHi
 
-            for i in range(bott - 1, top - 1, -1):
-                res.append(matrix[i][l])
-            #udpate left bound
-            l += 1
+            if currentDirection == 0:
+                rowsLo += 1
+            elif currentDirection == 1:
+                colsHi -= 1
+            elif currentDirection == 2:
+                rowsHi -= 1
+            else:
+                colsLo += 1
 
+            currentDirection = (currentDirection + 1) % 4
 
+        loc = (0, 0)
 
-        return res
-        
+        def isValidLoc(di, dj):
+            return rowsLo <= loc[0] + di < rowsHi and colsLo <= loc[1] + dj < colsHi
+        def nextLoc():
+            nonlocal loc
+            di, dj = directions[currentDirection]
+            if isValidLoc(di, dj):
+                loc = (loc[0] + di, loc[1] + dj)
+                return True
+            nextDirection()
 
+            di, dj = directions[currentDirection]
+            if isValidLoc(di, dj):
+                loc = (loc[0] + di, loc[1] + dj)
+                return True
+            return False
+
+        result = []
+        while True:
+            result.append(matrix[loc[0]][loc[1]])
+            if not nextLoc():
+                break
+        return result

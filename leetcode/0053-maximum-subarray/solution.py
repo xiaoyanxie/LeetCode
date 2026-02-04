f697f8ea2535eac43f4e71d66431720f2c71295e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-04 19:43:02 +0000

    [LeetCode Sync] Runtime - 34 ms (75.25%), Memory - 31.7 MB (29.24%)

--- leetcode/0053-maximum-subarray/solution.py
+++ leetcode/0053-maximum-subarray/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #subproblem: max subarray ending with index i dp[i]
-        #base case: dp[0]
-        dp = [nums[0]]
-        for i in range(1, len(nums)):
-            dp.append(max(dp[i-1] + nums[i], nums[i]))
-        
-        return max(dp)
+        # [-2,1,-3,4,-1,2,1,-5,4]
+        #                      ^
+        # maxSum: 6
+        # sum: 5
 
+        currSum = -inf
+        maxSum = -inf
+        for n in nums:
+            currSum = max(currSum + n, n)
+            maxSum = max(currSum, maxSum)
+        return maxSum

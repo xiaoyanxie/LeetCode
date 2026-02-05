8056735ff7a1c6de9100acd9e7d3b8956059b1ef
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-05 01:13:40 +0000

    [LeetCode Sync] Runtime - 518 ms (76.82%), Memory - 19.8 MB (41.90%)

---
+++ leetcode/0322-coin-change/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        #subproblem: dp[remain] = min(dp[remain], 1+ dp[remain - c])
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for c in coins:
+                if (i - c) >= 0:
+                    dp[i] = min(dp[i], dp[i-c] + 1)               
+
+        if dp[amount] == amount + 1:
+            return -1
+        return dp[amount]
+

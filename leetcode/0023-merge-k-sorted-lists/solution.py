06ef450091f649f28acd5663c9f3cff1986c2c0e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-06 05:30:52 +0000

    [LeetCode Sync] Runtime - 16 ms (25.52%), Memory - 24.3 MB (5.08%)

--- leetcode/0023-merge-k-sorted-lists/solution.py
+++ leetcode/0023-merge-k-sorted-lists/solution.py
@@ -1,28 +1,27 @@
-import heapq
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # [1,4,5],
-        # [1,3,4],
+        # [1,4,5]
+        # [1,3,4]
         # [2,6]
-        
-        heap = [] # 
+        heap = [] # [1, 1, 3], [2, 2, 6], [4, 0, 5]
         for i, node in enumerate(lists):
             if not node:
                 continue
             heapq.heappush(heap, [node.val, i, node.next])
 
-        dummy = ListNode() # 1->1->2->3->4->4->5->6
+        dummy = ListNode()
         curr = dummy
         while heap:
-            num, i, nxt = heapq.heappop(heap) # [6, 2, 1]
+            num, i, nxt = heapq.heappop(heap) # [1, 0, 4]
             curr.next = ListNode(num)
             curr = curr.next
             if nxt:
-                heapq.heappush(heap, [nxt.val, i, nxt.next])
-        
+                heapq.heappush(heap, [nxt.val, i, nxt.next]) 
+
         return dummy.next

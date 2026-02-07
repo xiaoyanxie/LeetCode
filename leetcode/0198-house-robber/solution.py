2b950eaacacaf52e6f8100ad0c172a7f620ebd6e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-07 21:03:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (14.96%)

---
+++ leetcode/0198-house-robber/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+        
+        dp = [0] * (len(nums) + 1)
+        dp[0] = 0
+        dp[1] = nums[0]
+        
+        for i in range(2, len(nums)+1):
+            dp[i] = max(dp[i - 2]+ nums[i-1], dp[i - 1])    
+
+        return dp[len(nums)]    
+
+        

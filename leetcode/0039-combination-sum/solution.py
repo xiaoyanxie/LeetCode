2c90a154a0abfe1a3823684db38d4499f3d5a652
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-08 00:28:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.7 MB (38.57%)

--- leetcode/0039-combination-sum/solution.py
+++ leetcode/0039-combination-sum/solution.py
@@ -1,25 +1,29 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         # [2,3,6,7]
+        # 2 (5) -> 2,3
+        #       -> 2 (3) -> 2, 3
+        #       -> 3 (1) -> None
 
-        # res = set()
-        res = []
+        combinations = []
         candidates.sort()
 
         def dfs(remaining, i, combination):
             if remaining == 0:
-                # res.add(tuple(sorted(combination)))
-                res.append(list(combination))
-            
-            for j in range(i, len(candidates)):
-                c = candidates[j]
+                combinations.append(list(combination))
+                return
+            if remaining < 0:
+                return
+
+            # for c in candidates:
+            for i in range(i, len(candidates)):
+                c = candidates[i]
                 if c > remaining:
                     break
                 combination.append(c)
-                dfs(remaining - c, j, combination)
+                dfs(remaining - c, i, combination)
                 combination.pop()
 
         dfs(target, 0, [])
 
-        # return list(res)
-        return res
+        return combinations

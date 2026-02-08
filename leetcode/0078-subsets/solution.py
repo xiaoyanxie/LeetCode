9f661ce5052255a11c4ddc4cf69720c48b64dce3
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-08 15:11:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (39.66%)

--- leetcode/0078-subsets/solution.py
+++ leetcode/0078-subsets/solution.py
@@ -14,14 +14,18 @@ class Solution:
             
         # dfs(0, [])
         # return res
-
+        #nums[1,2]->res[[],[1],[1,2],[2]]
+        # i =0 start = 0
+        # i = 1 start = 1
+        #path = [1] []
+        #i = 1 start = 1
         res = []
-        def dfs(start, path):
+        def dfs(path, start):
             res.append(path.copy())
-
             for i in range(start, len(nums)):
                 path.append(nums[i])
-                dfs(i+1, path)
+                dfs(path, i+1)
                 path.pop()
-        dfs(0, [])
+
+        dfs([], 0)
         return res

dee1efef8bfb06d8b2cbda5bf987d2808e4002d1
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-08 00:15:06 +0000

    [LeetCode Sync] Runtime - 35 ms (44.81%), Memory - 22.6 MB (8.17%)

--- leetcode/0417-pacific-atlantic-water-flow/solution.py
+++ leetcode/0417-pacific-atlantic-water-flow/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacificFlowables = set()
+        atlanticFlowables = set()
         rows = len(heights)
         cols = len(heights[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-        pacificFlowables = set()
-        atlanticFlowables = set()
 
         def isValidLoc(i, j):
             return 0 <= i < rows and 0 <= j < cols
@@ -19,7 +18,7 @@ class Solution:
                 if heights[i][j] <= heights[ni][nj]:
                     climbUpHill(ni, nj, flowables)
 
-        # Pacific Ocean Beach
+        #pacific beach
         for i in range(rows):
             j = 0
             climbUpHill(i, j, pacificFlowables)
@@ -28,11 +27,11 @@ class Solution:
             i = 0
             climbUpHill(i, j, pacificFlowables)
 
-        # Atlantic Ocean Beach
+        #atlantic beach
         for i in range(rows):
             j = cols - 1
             climbUpHill(i, j, atlanticFlowables)
-        
+
         for j in range(cols):
             i = rows - 1
             climbUpHill(i, j, atlanticFlowables)

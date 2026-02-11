abbb40b5257269369a0f325db45e75f491040733
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-11 03:12:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.7 MB (5.15%)

--- leetcode/0057-insert-interval/solution.py
+++ leetcode/0057-insert-interval/solution.py
@@ -1,13 +1,36 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals.sort()
-        res = [intervals[0]]
-        for start, end in intervals[1:]:
-            if start > res[-1][1]:
-                res.append([start,end])
-            else:
-                res[-1][1] = max(end, res[-1][1])
-        return res
+        def merge(result):
+            merged = [result[0]]
+            for i in range(1, len(result)):
+                interval = result[i]
+                if merged[-1][1] >= interval[0]:
+                    #merge
+                    merged[-1][1] = max(merged[-1][1], interval[1])
+                else:
+                    merged.append(interval)
+            return merged
+    
+        # l = 0
+        # r = len(intervals)
+        # while l < r:
+        #     mid = l + (r - l) // 2
+        #     if intervals[mid][0] == newInterval[0]:
+        #         result = intervals[:mid] + [newInterval] + intervals[mid:]
+        #         return merge(result)
 
+        #     if intervals[mid][0] < newInterval[0]:
+        #         l = mid + 1
+        #     else:
+        #         r = mid
+        
+        # if 0 <= l < len(intervals) and intervals[l][0] >= newInterval[0]:
+        #     result = intervals[:l] + [newInterval] + intervals[l:]
+        #     return merge(result)
+        # else:
+        #     result = intervals[:l + 1] + [newInterval] + intervals[l + 1:]
+        #     return merge(result)
 
+        index = bisect.bisect_left(intervals, newInterval)
+        intervals.insert(index, newInterval)
+        return merge(intervals)

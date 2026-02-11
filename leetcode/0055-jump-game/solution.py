4646f7335f9b5e98688b208cd110a2d18a2b8433
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-11 06:49:40 +0000

    [LeetCode Sync] Runtime - 28 ms (37.57%), Memory - 20.3 MB (55.86%)

--- leetcode/0055-jump-game/solution.py
+++ leetcode/0055-jump-game/solution.py
@@ -1,16 +1,32 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # [2,3,1,1,4]
-        #. 2 4 3 4 0
+        """
+        [2,3,1,1,4]
+           ^
+        longest = 4
+        steps = 1
 
-        # [3,2,1,0,4]
-        #  3 3 3 3  
 
-        far = 0
-        for i, step in enumerate(nums):
-            if i > far:
+        [3,2,1,0,4]
+               ^
+        longest = 3
+        steps = 4
+
+        [2,4,2,1,1,1,0,1] 7
+                i
+        longest = 6
+        steps = 6
+        """
+
+        longest = 0
+        steps = 0
+        for i, s in enumerate(nums):
+            if i > longest:
                 return False
-            far = max(far, i + step)
-            if far >= len(nums) - 1:
+            longest = max(longest, steps + s)
+            steps += 1
+            # print(f'{longest}, {steps}')
+            if longest >= len(nums) - 1:
                 return True
-        return far >= len(nums) - 1
+        return True
+

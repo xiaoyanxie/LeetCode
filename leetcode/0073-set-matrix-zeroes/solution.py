790173fc80cb2042ea565ef20f8bdaf0aef27b74
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-13 00:07:04 +0000

    [LeetCode Sync] Runtime - 11 ms (6.95%), Memory - 20.6 MB (13.84%)

--- leetcode/0073-set-matrix-zeroes/solution.py
+++ leetcode/0073-set-matrix-zeroes/solution.py
@@ -3,26 +3,40 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        #for each cell, find 0 and check if the row/colum is 0, if not replace
-        zeroCol = set()
-        zeroRow = set()
-        row = len(matrix)
-        col = len(matrix[0])
-        for i in range(row):
-            for j in range(col):
-                if matrix[i][j] == 0:
-                    zeroRow.add(i)
-                    zeroCol.add(j)
+        vertical = [ (1, 0), (-1, 0) ]
+        horizontal = [ (0, 1), (0, -1) ]
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        def isValidLoc(i, j):
+            return 0 <= i < rows and 0 <= j < cols
+
+        def allZeros(i, j, directions):
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if isValidLoc(ni, nj) and matrix[ni][nj] != 0:
+                    return False
+            return True
+
+        def markZeros(i, j, directions):
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                while isValidLoc(ni, nj):
+                    if matrix[ni][nj] != 0:
+                        matrix[ni][nj] = -inf
+                    ni, nj = ni + di, nj + dj
+    
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] != 0: continue
+            
+                if not allZeros(i, j, vertical):
+                    markZeros(i, j, vertical)
+                if not allZeros(i, j, horizontal):
+                    markZeros(i, j, horizontal)
         
-
-        for i in range(row):
-            if i in zeroRow:
-                for j in range(col):
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == -inf:
                     matrix[i][j] = 0
-                    
-        for j in range(col):
-            if j in zeroCol:
-                for i in range(row):
-                    matrix[i][j] = 0     
-
-

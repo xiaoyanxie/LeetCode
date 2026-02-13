285b627e2fc9b4513a261aa080fa92e8fa39c24f
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-13 23:18:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (45.55%)

--- leetcode/0213-house-robber-ii/solution.py
+++ leetcode/0213-house-robber-ii/solution.py
@@ -1,27 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #subproblem: dp[i] = max(dp[i-2]+nums[i-1], dp[i-1])
-        #circle: 1. range(1, len(nums)) 2. range(2, len(nums) + 1)
-        #base case: dp[1] = 1 
-        if len(nums) <= 2:
-            return max(nums)
-        
-        dp = [0] * (len(nums) + 1)
+        if len(nums) == 1: return nums[0]
+        def rob(houses):
+            prev2, prev1 = 0, 0
+            for val in houses:
+                amt = max(prev1, val + prev2)
+                prev2 = prev1
+                prev1 = amt
+            return prev1
 
-        dp[0] = 0 
-        dp[1] = nums[0]
-
-        # get rid of the last house
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 2] + nums[i - 1], dp[i - 1])
-        tmp = dp[len(nums) - 1]
-
-        dp = [0] * (len(nums) + 1)
-        dp[0] = 0
-        dp[1] = 0
-        dp[2] = nums[1]
-        #get rid of the first house
-        for i in range(3, len(nums) + 1):
-            dp[i] = max(dp[i - 2] + nums[i - 1], dp[i - 1])
-
-        return max(tmp,dp[len(nums)])
+        return max(rob(nums[1:]), rob(nums[0:-1]))

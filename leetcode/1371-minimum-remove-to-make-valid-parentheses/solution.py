6298ba23db49ace1868cd25f587ccb22832d26b4
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-13 23:05:49 +0000

    [LeetCode Sync] Runtime - 42 ms (61.76%), Memory - 20.9 MB (39.37%)

--- leetcode/1371-minimum-remove-to-make-valid-parentheses/solution.py
+++ leetcode/1371-minimum-remove-to-make-valid-parentheses/solution.py
@@ -1,63 +1,38 @@
 class Solution:
+    #left but no rihgt
+    #right but no left
+    #first right then left
+    #first right then left
+
     def minRemoveToMakeValid(self, s: str) -> str:
-        s = list(s)
-        stack = []
-        #First remove unpaired ')'
-        for i in range(len(s)):
-            if s[i] == "(":
-                stack.append(i)
-            elif s[i] == ")":
-                if stack:
-                    stack.pop()
-                else:
-                    s[i] = ''
-        #Second remove unpaired '('
-        while stack:
-            s[stack.pop()] = ''
-        
-        return ''.join(s)
-        # #change my original solution to helper function to reduce the space 
-        # def delete_invalid_paren(s, open_symbol, close_symbol):
-        #     count = 0
-        #     result = []
-        #     for char in s:
-        #         if char == open_symbol:
-        #             count += 1
-        #         elif char == close_symbol:
-        #             if count == 0:
-        #                 continue
-        #             else:
-        #                 count -= 1
-        #         result.append(char)
+        left = 0
+        right = 0
+        remove =set()
+        for i, letter in enumerate(s):
+            if letter == ")":
+                right += 1
+                if right > left:
+                    remove.add(i)
+                    right -= 1
+            elif letter == "(":
+                left += 1
+   
+        if left != right:
+        #     if right == 0:
+        #         return ""
+    
+            more = left - right
             
-        #     return "".join(result)
+            for i, letter in enumerate(s[::-1]):
+                if more == 0:
+                    break
+                if letter == "(":
+                    remove.add(len(s) - i - 1)
+                    more -= 1
+        res = []
+        for i, letter in enumerate(s):
+            if i not in remove:
+                res.append(letter)
             
-        # s = delete_invalid_paren(s, "(", ")")
-        # s = delete_invalid_paren(s[::-1], ")", "(")
-        # return s[::-1]
-
-        
-
-#below is my original method with two list which has good runtime yet bad space.
-        # count = 0
-        # result = []
-
-        # for alpha in s:
-        #     if alpha == "(":
-        #         count+=1
-        #     elif alpha == ")":
-        #         if count == 0:
-        #             continue
-        #         count-=1
-        #     result.append(alpha)
-
-        # update = []
-        # openNeeded = count
-
-        # for alpha in reversed(result):
-        #     if alpha == "(" and openNeeded > 0:
-        #         openNeeded -=1
-        #         continue
-        #     update.append(alpha)
-
-        # return "".join(reversed(update))
+        return ''.join(res)
+            

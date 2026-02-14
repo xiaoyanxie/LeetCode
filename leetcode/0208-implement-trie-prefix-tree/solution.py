090a0b0c312dec9109a0f44e99a9a52bbe9cad24
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-14 01:15:14 +0000

    [LeetCode Sync] Runtime - 39 ms (63.19%), Memory - 34 MB (54.75%)

--- leetcode/0208-implement-trie-prefix-tree/solution.py
+++ leetcode/0208-implement-trie-prefix-tree/solution.py
@@ -1,46 +1,38 @@
 class Node:
-    def __init__(self, char):
-        self.c = char
-        self.isWord = False
+    def __init__(self):
         self.children = {}
+        self.isWord = False
 
 class Trie:
 
     def __init__(self):
-        self.root = Node(None)
+        self.root = Node()
 
     def insert(self, word: str) -> None:
-        if not word:
-            return
         curr = self.root
         for i, c in enumerate(word):
             if c in curr.children:
                 curr = curr.children[c]
                 continue
-            curr.children[c] = Node(c)
+            curr.children[c] = Node()
             curr = curr.children[c]
         curr.isWord = True
 
     def search(self, word: str) -> bool:
         curr = self.root
         for i, c in enumerate(word):
-            if c in curr.children:
-                curr = curr.children[c]
-                continue
-            else:
+            if c not in curr.children:
                 return False
+            curr = curr.children[c]
         return curr.isWord
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.root
         for i, c in enumerate(prefix):
-            if c in curr.children:
-                curr = curr.children[c]
-                continue
-            else:
+            if c not in curr.children:
                 return False
+            curr = curr.children[c]
         return True
-
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()

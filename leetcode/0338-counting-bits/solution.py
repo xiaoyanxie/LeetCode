38b53a3c43ec8553ff38e5979cd6745e7f872f1b
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-14 02:38:23 +0000

    [LeetCode Sync] Runtime - 3 ms (95.58%), Memory - 20.5 MB (15.25%)

--- leetcode/0338-counting-bits/solution.py
+++ leetcode/0338-counting-bits/solution.py
@@ -1,16 +1,20 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        """
+        0 - 0b0
+        1 - 0b1
+        2 - 0b10
+        3 - 0b11
+        4 - 0b100
+        5 - 0b101
+        6 - 0b110
+        7 - 0b111
+        8 - 0b1000
+        9 - 0b1001
+        10- 0b1010
+        11- 0b1011
+        """
         ans = [0] * (n + 1)
-
-        def count(i):
-            cnt = 0
-            while i > 0:
-                if i & 1 > 0:
-                    cnt += 1
-                i >>= 1
-            return cnt
-
-        for i in range(len(ans)):
-            ans[i] = count(i)
-
+        for i in range(n + 1):
+            ans[i] = ans[i >> 1] + (i & 0b1)
         return ans

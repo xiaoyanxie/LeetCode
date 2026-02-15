97937c25cc2e83a9db5b8f489fbffecd61464f56
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-15 21:25:48 +0000

    [LeetCode Sync] Runtime - 11 ms (35.23%), Memory - 19.5 MB (71.71%)

--- leetcode/0125-valid-palindrome/solution.py
+++ leetcode/0125-valid-palindrome/solution.py
@@ -1,22 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         i, j = 0, len(s) - 1
-
-        def isEnglish(c):
-            return ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z')
-
-        def isNumber(c):
-            return ord('0') <= ord(c) <= ord('9')
-
-        while i <= j:
-            if not isEnglish(s[i]) and not isNumber(s[i]):
+        while i < j:
+            if not s[i].isalnum():
                 i += 1
-                continue
-            if not isEnglish(s[j]) and not isNumber(s[j]):
+            elif not s[j].isalnum():
                 j -= 1
-                continue
-            if s[i].lower() != s[j].lower():
+            elif s[i].lower() != s[j].lower():
                 return False
-            i += 1
-            j -= 1
+            else:
+                i += 1
+                j -= 1
         return True
+            

b5670d47e3739875eaa367bbc659ddfe69abbfbd
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-15 20:16:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.5 MB (28.43%)

--- leetcode/0252-meeting-rooms/solution.py
+++ leetcode/0252-meeting-rooms/solution.py
@@ -2,12 +2,11 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         if not intervals:
             return True
-        
         intervals.sort(key=lambda x: x[1])
         prev = intervals[0]
         for i in range(1, len(intervals)):
             curr = intervals[i]
-            if curr[0] < prev[1]:
+            if prev[1] > curr[0]:
                 return False
             prev = curr
         return True

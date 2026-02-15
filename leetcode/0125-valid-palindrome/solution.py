5b1de658a73d452d7851231c1724979c5acda44d
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-15 02:53:26 +0000

    [LeetCode Sync] Runtime - 23 ms (5.44%), Memory - 19.8 MB (44.11%)

--- leetcode/0125-valid-palindrome/solution.py
+++ leetcode/0125-valid-palindrome/solution.py
@@ -1,27 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # initialize two pointers
-        i = 0
-        j = len(s) - 1
-        #iterate over the whole string to compare 
-        #abbbba
-        # ^  ^
-        #->   <-
-        while i < j:
-            while i < j and not s[i].isalnum():
+        i, j = 0, len(s) - 1
+
+        def isEnglish(c):
+            return ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z')
+
+        def isNumber(c):
+            return ord('0') <= ord(c) <= ord('9')
+
+        while i <= j:
+            if not isEnglish(s[i]) and not isNumber(s[i]):
                 i += 1
-            while i < j and not s[j].isalnum():
+                continue
+            if not isEnglish(s[j]) and not isNumber(s[j]):
                 j -= 1
+                continue
             if s[i].lower() != s[j].lower():
                 return False
             i += 1
             j -= 1
         return True
-
-
-
-# The two-pointer algorithm runs in O(n) time because each character in the string is checked at most once by one of the pointers. It uses only O(1) extra space since it only needs to keep track of two indices (the left and right pointers).
-
-
-
-

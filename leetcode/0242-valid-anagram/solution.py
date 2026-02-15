721565b714bcf697846287cb51c0da5d11d92974
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-15 19:44:27 +0000

    [LeetCode Sync] Runtime - 7 ms (90.80%), Memory - 19.5 MB (53.19%)

--- leetcode/0242-valid-anagram/solution.py
+++ leetcode/0242-valid-anagram/solution.py
@@ -1,15 +1,3 @@
-from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        mp = defaultdict(int)
-        for c in s:
-            mp[c] += 1
-        
-        for c in t:
-            if c not in mp:
-                return False
-            mp[c] -= 1
-            if mp[c] == 0:
-                del mp[c]
-        
-        return len(mp) == 0
+        return Counter(s) == Counter(t)

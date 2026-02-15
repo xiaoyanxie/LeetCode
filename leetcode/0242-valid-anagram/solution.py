a7779b874a05aa7e3764a5bd5f5d0f9a66dc1ff4
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-15 02:47:51 +0000

    [LeetCode Sync] Runtime - 11 ms (75.78%), Memory - 19.5 MB (51.96%)

--- leetcode/0242-valid-anagram/solution.py
+++ leetcode/0242-valid-anagram/solution.py
@@ -1,23 +1,15 @@
+from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        # ht = {}
-        # for i in s:
-        #     # ht[i] = ht.get(i,0) + 1
-        #     #ht.get取i的value，如果没有就返回0
-        #     # + 1 累加次数
-        #     #ht[i]更新当前value
-        #     if i in ht:
-        #         ht[i] += 1
-        #     else:
-        #         ht[i] = 1
-        # for n in t:
-        #     val = ht.get(n)
-        #     if val == 0 or val == None:
-        #         return False
-        #     else:
-        #         ht[n] -= 1
-        # return True  
-        return Counter(s) == Counter(t)
+        mp = defaultdict(int)
+        for c in s:
+            mp[c] += 1
+        
+        for c in t:
+            if c not in mp:
+                return False
+            mp[c] -= 1
+            if mp[c] == 0:
+                del mp[c]
+        
+        return len(mp) == 0

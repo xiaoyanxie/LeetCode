aa01e811227d59a4b4660a10d663b5eb5bfcd752
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-15 21:23:45 +0000

    [LeetCode Sync] Runtime - 7 ms (21.93%), Memory - 19.3 MB (57.45%)

--- leetcode/0139-word-break/solution.py
+++ leetcode/0139-word-break/solution.py
@@ -1,31 +1,13 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_word = False
-
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        root = TrieNode()
-        for word in wordDict:
-            node = root
-            for c in word:
-                if c not in node.children:
-                    node.children[c] = TrieNode()
-                node = node.children[c]
-            node.is_word = True
-    
-        @cache
-        def find(i) -> bool:
-            # if i == len(s):
-            #     return True
-            
-            node = root
-            for j in range(i, len(s)):
-                if s[j] not in node.children:
-                    break
-                node = node.children[s[j]]
-                if node.is_word and (j == len(s) - 1 or find(j + 1)):
-                    return True
-            return False
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:        
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
 
-        return find(0)
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] == True and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        
+        return dp[len(s)]
+

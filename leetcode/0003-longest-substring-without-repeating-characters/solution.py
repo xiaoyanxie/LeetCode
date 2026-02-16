e84518003adbaf013b02ad6e2314942b3bec0358
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-16 00:22:25 +0000

    [LeetCode Sync] Runtime - 15 ms (54.99%), Memory - 19.4 MB (50.02%)

--- leetcode/0003-longest-substring-without-repeating-characters/solution.py
+++ leetcode/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,19 +1,18 @@
+from collections import defaultdict
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # s = "abcabcbb"
-        #             ^
+        l = 0
+        freq = defaultdict(int)
+        maxlen = 0
+        for r, c in enumerate(s):
 
-        l = 0 # 7
-        seen = {} # {a: 3, b: 7, c: 5}
-        maxLen = 0 # 1
-        for r in range(len(s)): # r = 7
-            if s[r] in seen and seen[s[r]] >= l:
-                l = seen[s[r]] + 1
-            
-            seen[s[r]] = r
+            freq[c] += 1
 
-            newLen = r - l + 1
-            if newLen > maxLen:
-                maxLen = newLen
+            while freq[c] > 1:
+                freq[s[l]] -= 1
+                l += 1
 
-        return maxLen
+            maxlen = max(maxlen, r - l + 1)
+        
+        return maxlen
+

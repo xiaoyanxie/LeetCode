c28b02aa62e0968e291c3ddfdd238745f49d3440
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-16 00:28:26 +0000

    [LeetCode Sync] Runtime - 29 ms (82.63%), Memory - 20.8 MB (51.46%)

--- leetcode/1371-minimum-remove-to-make-valid-parentheses/solution.py
+++ leetcode/1371-minimum-remove-to-make-valid-parentheses/solution.py
@@ -1,38 +1,18 @@
 class Solution:
-    #left but no rihgt
-    #right but no left
-    #first right then left
-    #first right then left
-
     def minRemoveToMakeValid(self, s: str) -> str:
-        left = 0
-        right = 0
-        remove =set()
-        for i, letter in enumerate(s):
-            if letter == ")":
-                right += 1
-                if right > left:
-                    remove.add(i)
-                    right -= 1
-            elif letter == "(":
-                left += 1
-   
-        if left != right:
-        #     if right == 0:
-        #         return ""
-    
-            more = left - right
-            
-            for i, letter in enumerate(s[::-1]):
-                if more == 0:
-                    break
-                if letter == "(":
-                    remove.add(len(s) - i - 1)
-                    more -= 1
-        res = []
-        for i, letter in enumerate(s):
-            if i not in remove:
-                res.append(letter)
-            
-        return ''.join(res)
-            
+        stack = []
+        ret = []
+
+        for c in s:
+            if c != ')':
+                ret.append(c)
+                if c == '(':
+                    stack.append(len(ret) - 1)
+            elif stack:
+                stack.pop()
+                ret.append(c)
+        
+        while stack:
+            ret[stack.pop()] = ''
+
+        return ''.join(ret)

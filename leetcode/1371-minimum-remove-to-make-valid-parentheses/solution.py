b283dcca58fcd9c0ff330ad2132d4e05dac3b89e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-17 20:01:28 +0000

    [LeetCode Sync] Runtime - 25 ms (92.59%), Memory - 21 MB (31.93%)

--- leetcode/1371-minimum-remove-to-make-valid-parentheses/solution.py
+++ leetcode/1371-minimum-remove-to-make-valid-parentheses/solution.py
@@ -1,18 +1,33 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        """
+        lee(t(c)o)de)
+                    ^
+
+        ret: lee(t(c)o)de
+        stack: []
+
+        ())a((
+             ^
+
+        ret: ()a  
+        stack: [3,4]
+        """
+
+        # Pass 1:
         stack = []
         ret = []
-
         for c in s:
             if c != ')':
                 ret.append(c)
                 if c == '(':
                     stack.append(len(ret) - 1)
             elif stack:
-                stack.pop()
                 ret.append(c)
+                stack.pop()
         
-        while stack:
-            ret[stack.pop()] = ''
-
+        # Pass 2:
+        for i in stack:
+            ret[i] = ''
+        
         return ''.join(ret)

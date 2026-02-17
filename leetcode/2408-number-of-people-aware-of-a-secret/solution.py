4dae19805f8faa4e4c185eb74663923b68e1f789
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-17 20:26:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (59.54%)

--- leetcode/2408-number-of-people-aware-of-a-secret/solution.py
+++ leetcode/2408-number-of-people-aware-of-a-secret/solution.py
@@ -1,18 +1,29 @@
 from collections import deque
-
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
-        activeSharing = 0
+        MOD = 10**9 + 7
+        
+        """
+        1     0     1     1     1     2
+        day1  day2  day3  day4  day5  day6
+        ^
+    ^   ^
+        """
+
+        # sliding window
         queue = deque([1])
-        for i in range(1, n):
+        activeSharing = 0
+        for day in range(1, n):
+            # shrink the queue
             if len(queue) == forget:
-                # handle forget
                 activeSharing -= queue.popleft()
             
-            wokeUp = len(queue) - delay
-            if wokeUp >= 0:
-                activeSharing += queue[wokeUp]
-            queue.append(activeSharing)
-        
-        return sum(queue) % (10**9 + 7)
+            lastActive = len(queue) - delay
+            if lastActive >= 0:
+                activeSharing += queue[lastActive]
             
+            # print(f'day{day + 1}, {activeSharing} active sharing')
+            queue.append(activeSharing)
+
+        return sum(queue) % MOD
+

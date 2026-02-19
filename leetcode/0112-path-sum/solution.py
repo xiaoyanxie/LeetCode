ee5dc1f9c3a2823117fdd9153eeae6cdfea5f154
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-19 05:59:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.3 MB (17.62%)

--- leetcode/0112-path-sum/solution.py
+++ leetcode/0112-path-sum/solution.py
@@ -6,30 +6,28 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # if not root:
-        #     return False
-        #     #augmented node, includes sum from root to it
-        # stack=[(root, root.val)]
-        # while stack:
-        #     node, val = stack.pop()
-        #     if not node.left and not node.right:
-        #         if val == targetSum:
-        #             return True
-        #     if node.left:
-        #         stack.append((node.left, val+node.left.val))
-        #     if node.right:
-        #         stack.append((node.right, val+node.right.val))
-        # return False
-        
         if not root:
             return False
-        if not root.left and not root.right:
-            if root.val == targetSum:
+        
+        # if not root.left and not root.right:
+        #     if root.val == targetSum:
+        #         return True
+        #     return False
+        
+        # return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+        
+        stack =[(root, targetSum-root.val)]
+        while stack:
+            node, sum = stack.pop()
+            if sum == 0 and not node.left and not node.right:
                 return True
-        
-        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
-        
-        
+            
+            if node.right:
+                stack.append((node.right, sum - node.right.val))
+                
+            if node.left:
+                stack.append((node.left, sum - node.left.val))
+            
+        return False
 
-
-
+            

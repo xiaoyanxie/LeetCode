2efbf179b6cb1861986efdf8cd9bc1126f8c93c1
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-22 06:12:57 +0000

    [LeetCode Sync] Runtime - 2 ms (71.80%), Memory - 19.4 MB (35.06%)

--- leetcode/0002-add-two-numbers/solution.py
+++ leetcode/0002-add-two-numbers/solution.py
@@ -5,37 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # l1 = [9,9,9], l2 = [9,9]
-        # sumi = 0
-
-
-        carry = 0 # 1
-        dummyHead = ListNode() # () -> (8) -> (9) -> (0) -> (1)
-        curr = dummyHead # 
+        root = ListNode()
+        curr = root
+        carry = 0
         while l1 or l2:
-            if l1 and l2:
-                sumi = l1.val + l2.val + carry
-            elif l1:
-                sumi = l1.val + carry
-            else:
-                sumi = l2.val + carry
-            
-            if sumi > 9:
+            if not l1 and not l2:
+                break
+            val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            if val >= 10:
                 carry = 1
-                sumi = sumi - 10
+                val -= 10
             else:
                 carry = 0
-            
-            next = ListNode(sumi)
-            curr.next = next
-            curr = next
-
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-        
+            curr.next = ListNode(val)
+            curr = curr.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
         if carry == 1:
             curr.next = ListNode(1)
-
-        return dummyHead.next
+        return root.next

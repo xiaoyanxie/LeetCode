0a5efadbc2ef532a9eb601be3ef6f28146a6b74a
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-22 02:06:30 +0000

    [LeetCode Sync] Runtime - 259 ms (64.95%), Memory - 19.4 MB (61.52%)

--- leetcode/0005-longest-palindromic-substring/solution.py
+++ leetcode/0005-longest-palindromic-substring/solution.py
@@ -1,27 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # abaabba
-
-        def expand(i, j):
-            while i >= 0 and j < len(s) and s[i] == s[j]:
+        def expand(i, j) -> str:
+            if s[j] != s[i]:
+                return ''
+            while 0 <= i and j < len(s) and s[i] == s[j]:
                 i -= 1
                 j += 1
-            return (i + 1, j)
+            return s[i + 1: j]
 
-        best = ""
+        best = ''
         for i in range(len(s)):
-            print(f"search from s[{i}] = {s[i]}")
-            start, end = expand(i, i)
-            print(f"found {s[start:end]}")
-            if end - start > len(best):
-                best = s[start:end]
-            if i + 1 == len(s):
-                continue
-
-            print(f"search from s[{i}] = {s[i]} and s[{i + 1}] = {s[i + 1]}")
-            start1, end1 = expand(i, i + 1)
-            print(f"found {s[start1:end1]}")
-            if end1 - start1 > len(best):
-                best = s[start1:end1]
-        
+            s1 = expand(i, i)
+            if len(s1) > len(best):
+                best = s1
+            if i > 0:
+                s2 = expand(i - 1, i)
+                if len(s2) > len(best):
+                    best = s2
         return best

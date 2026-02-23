e06b3fa4eb24e4a47053fe79a85f6a2e4ab3d76e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-23 19:08:56 +0000

    [LeetCode Sync] Runtime - 31 ms (88.78%), Memory - 21.5 MB (69.97%)

--- leetcode/1006-vowel-spellchecker/solution.py
+++ leetcode/1006-vowel-spellchecker/solution.py
@@ -1,44 +1,40 @@
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
         """
-        1. lookup the set(wordList), if a match, add to result
-
-        2. lookup the case insensitive map, if a match, return the first match in the wordlist: for example: HARE matches hare -> [hare, Hare], then return hare
-
-        3. mask the vowel, lookup the masked vowel map, if there is a match, return the first match like 2)
+        1. exact match
+        2. case insensetive match
+        3. vowel insensetive match
         """
+        original = set()
+        caseInsensetive = {}
+        vowelInsensetive = {}
+        vowels = set(('a', 'e', 'i', 'o', 'u'))
 
-        original = set(wordlist)
-        caseInsensitive = {}
-        deVowels = {}
+        def devowel(word):
+            return ''.join([
+                c if c not in vowels else '*' for c in word.lower()
+            ])
 
-        def deVowel(w):
-            ret = []
-            for c in w:
-                if c.lower() in ('a', 'e', 'i', 'o', 'u'):
-                    ret.append('*')
-                else:
-                    ret.append(c.lower())
-            return ''.join(ret)
-
-        for w in wordlist:
-            w0 = w.lower()
-            if w0 not in caseInsensitive:
-                caseInsensitive[w0] = w
-            w1 = deVowel(w)
-            if w1 not in deVowels:
-                deVowels[w1] = w
+        for word in wordlist:
+            original.add(word)
+            lower = word.lower()
+            devoweled = devowel(word)
+            if lower not in caseInsensetive:
+                caseInsensetive[lower] = word
+            if devoweled not in vowelInsensetive:
+                vowelInsensetive[devoweled] = word
         
         ret = []
         for query in queries:
+            lower = query.lower()
+            devoweled = devowel(query)
+
             if query in original:
                 ret.append(query)
-            elif query.lower() in caseInsensitive:
-                ret.append(caseInsensitive[query.lower()])
+            elif lower in caseInsensetive:
+                ret.append(caseInsensetive[lower])
+            elif devoweled in vowelInsensetive:
+                ret.append(vowelInsensetive[devoweled])
             else:
-                w = deVowel(query)
-                if w in deVowels:
-                    ret.append(deVowels[w])
-                else:
-                    ret.append('')
+                ret.append('')
         return ret

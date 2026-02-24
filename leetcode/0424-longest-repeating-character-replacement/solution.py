88f6f9a90c9c7c7363b93e0d1a7df19d1f929787
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-24 22:34:37 +0000

    [LeetCode Sync] Runtime - 79 ms (80.59%), Memory - 19.9 MB (13.32%)

--- leetcode/0424-longest-repeating-character-replacement/solution.py
+++ leetcode/0424-longest-repeating-character-replacement/solution.py
@@ -1,14 +1,19 @@
-from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        """
+        AABABBA
+        l
+         r
+        """
         freq = defaultdict(int)
-        maxLen = 0
         l = 0
+        best = 0
+        maxfreq = 0
         for r, c in enumerate(s):
             freq[c] += 1
-            while r - l + 1 - max(freq.values()) > k:
+            maxfreq = max(freq[c], maxfreq)
+            while (r - l + 1) - maxfreq > k:
                 freq[s[l]] -= 1
                 l += 1
-
-            maxLen = max(maxLen, r - l + 1)
-        return maxLen
+            best = max(best, r - l + 1)
+        return best

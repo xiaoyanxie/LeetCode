ba82bc2e8212fcd88c8f295085e29dfb69f64a55
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-24 17:21:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.2 MB (6.31%)

--- leetcode/0098-validate-binary-search-tree/solution.py
+++ leetcode/0098-validate-binary-search-tree/solution.py
@@ -5,6 +5,8 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.pre = float('-inf')
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         def isValid(node, nmin, nmax):
             if not node:
@@ -13,6 +15,16 @@ class Solution:
                 return False
             return isValid(node.left, nmin, node.val) and isValid(node.right, node.val, nmax)
 
-        if not root:
-            return True
+        # if not root:
+        #     return True
         return isValid(root, float('-inf'), float('inf'))
+        # if not root:
+        #     return True
+        # if not self.isValidBST(root.left):
+        #     return False
+        # if root.val <= self.pre:
+        #     return False
+        # self.pre = root.val
+        # return self.isValidBST(root.right)
+    
+

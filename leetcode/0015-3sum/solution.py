22170a91d44dbd62126242e56f250f41ffe71c9d
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-25 04:42:32 +0000

    [LeetCode Sync] Runtime - 1166 ms (12.91%), Memory - 22.5 MB (28.64%)

--- leetcode/0015-3sum/solution.py
+++ leetcode/0015-3sum/solution.py
@@ -1,29 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = []
-        n = len(nums)
 
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            if nums[i] > 0:
-                break
-            l = i + 1
-            r = n - 1
-
-            while l < r:
-                if nums[l] + nums[r] + nums[i] == 0:
-                    res.append([nums[i], nums[l], nums[r]])
-                    
-                    while l<r and nums[l+1] == nums[l]:
-                        l += 1
-                    while l<r and nums[r-1] == nums[r]:
-                        r -= 1
-                    l += 1
-                    r -= 1
-                elif nums[l] + nums[r] > -nums[i]:
-                    r -= 1
+        def twoSum(exclude, n):
+            ret = []
+            i, j = exclude + 1, len(nums) - 1
+            while i < j:
+                a, b = nums[i], nums[j]
+                if a + b + n == 0:
+                    ret.append([a, b, n])
+                    while i < j and nums[i] == a:
+                        i += 1
+                    while i < j and nums[j] == b:
+                        j -= 1
+                elif a + b + n < 0:
+                    while i < j and nums[i] == a:
+                        i += 1
                 else:
-                    l += 1
-        return res
+                    while i < j and nums[j] == b:
+                        j -= 1
+            return ret
+        
+        ret = []
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
+                continue
+            ret += twoSum(i, n)
+        return ret

e455bbfdb761c172879e750c6364b52d14c7a30b
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-26 16:32:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.4 MB (66.97%)

--- leetcode/0268-missing-number/solution.py
+++ leetcode/0268-missing-number/solution.py
@@ -1,6 +1,23 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        actual_sum = sum(nums)
-        n = len(nums)
-        expected_sum = int((n * (n + 1)) / 2)
-        return expected_sum - actual_sum
+        # if len(nums) == 1:
+        #     if nums[0] == 0:
+        #         return 1
+        #     else:
+        #         return 0
+
+        # nums.sort()
+        # if nums[0] != 0:
+        #     return 0
+        # for i in range(1, len(nums)):
+        #     if nums[i] - nums[i - 1] != 1:
+        #         return i
+        
+        # return len(nums)
+        target = (0 + len(nums)) * (len(nums) + 1) // 2
+    
+        for num in nums:
+            target -= num
+        return target
+
+

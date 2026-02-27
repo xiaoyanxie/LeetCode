66de427c9cc94e5523fd46d88a96da6ade8e012b
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-27 04:56:12 +0000

    [LeetCode Sync] Runtime - 35 ms (64.95%), Memory - 31.7 MB (19.69%)

--- leetcode/0053-maximum-subarray/solution.py
+++ leetcode/0053-maximum-subarray/solution.py
@@ -1,13 +1,20 @@
+from collections import defaultdict
+import heapq
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # [-2,1,-3,4,-1,2,1,-5,4]
-        #                      ^
-        # maxSum: 6
-        # sum: 5
+        """
 
-        currSum = -inf
-        maxSum = -inf
-        for n in nums:
-            currSum = max(currSum + n, n)
-            maxSum = max(currSum, maxSum)
-        return maxSum
+        i -> j: sum(nums[i:j + 1])
+        """
+
+        best = -inf
+        minRunningSum = 0
+        runningSum = 0
+        for i, n in enumerate(nums):
+            runningSum += n
+            if runningSum - minRunningSum > best:
+                best = runningSum - minRunningSum
+            # heapq.heappush(runningSums, runningSum)
+            if minRunningSum > runningSum:
+                minRunningSum = runningSum
+        return best

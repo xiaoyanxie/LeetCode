e897f8afddd1c72c9d423a43fd9fe908c1b2f56a
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-27 06:18:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (16.57%)

--- leetcode/0070-climbing-stairs/solution.py
+++ leetcode/0070-climbing-stairs/solution.py
@@ -1,26 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        # @cache
-        # def dfs(remain):
-        #     if remain == 0:
-        #         return 1
-        #     if remain >= 2:
-        #         return dfs(remain-2) + dfs(remain-1)
-        #     return dfs(remain-1)
+        @cache
+        def climb(steps):
+            if steps == n:
+                return 1
             
-        # return dfs(n)
-
-        #subproblem: how many kinds of ways i have to reach cur place dp[i] = dp[i-1] + dp[i-2]
-        #base case: dp[0] = 0, dp[1] = 1
-
-        #n = 2
-        mem = [0] * (n + 1) #[1,1,0,0]
-        mem[0] = 1
-        mem[1] = 1
-
-        for i in range(2,n+1):
-            mem[i] = mem[i-1] + mem[i-2]
-
-        return mem[n]
-
+            ways = 0
+            if steps + 1 <= n:
+                ways += climb(steps + 1)
+            if steps + 2 <= n:
+                ways += climb(steps + 2)
+            return ways
+        
+        return climb(0)

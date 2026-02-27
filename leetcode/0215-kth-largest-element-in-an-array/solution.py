4cb4dde2e8c98686d10f1b50878340ea1614e16c
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-27 07:26:52 +0000

    [LeetCode Sync] Runtime - 22 ms (99.63%), Memory - 31.4 MB (11.54%)

--- leetcode/0215-kth-largest-element-in-an-array/solution.py
+++ leetcode/0215-kth-largest-element-in-an-array/solution.py
@@ -1,16 +1,26 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            if len(heap) < k:
-                heapq.heappush(heap, num)
-            elif len(heap) >= k:
-                if num < heap[0]:
-                    continue
-                else:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, num)
+        pivot = random.choice(nums)
         
-        return heap[0]
-
-
+        left, mid, right = [], [], []
+        for n in nums:
+            if n < pivot:
+                left.append(n)
+            elif n > pivot:
+                right.append(n)
+            else:
+                mid.append(n)
+        
+        """
+          L         M      R
+        | 1,2,3 | 4,4,4 | 5,6,7 |
+           k: self.findKthLargest(left, k - len(mid) - len(right))
+                   k: return pivot
+                           k: self.findKthLargest(right, k)
+        """
+        if len(right) < k <= len(mid) + len(right):
+            return pivot
+        elif k <= len(right):
+            return self.findKthLargest(right, k)
+        else:
+            return self.findKthLargest(left, k - len(mid) - len(right))

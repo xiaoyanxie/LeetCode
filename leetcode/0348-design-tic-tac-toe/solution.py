fa5fe664a03ca68f3412fa6687675325b48fb781
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-02-28 07:29:03 +0000

    [LeetCode Sync] Runtime - 68 ms (97.49%), Memory - 21.6 MB (74.09%)

--- leetcode/0348-design-tic-tac-toe/solution.py
+++ leetcode/0348-design-tic-tac-toe/solution.py
@@ -1,32 +1,22 @@
+from collections import defaultdict
 class TicTacToe:
 
     def __init__(self, n: int):
         self.n = n
-        # player -> row -> cnt
-        self.rows = {1: defaultdict(int), 2: defaultdict(int)} # {1 -> 0 -> 1, 2 -> 1 -> 1, 1 -> 2 -> 1, 2 -> 0 -> 1}
-        # player -> col -> cnt
-        self.cols = {1: defaultdict(int), 2: defaultdict(int)} # {1 -> 0 -> 1, 2 -> 1 -> 1, 1 -> 2 -> 1, 2 -> 2 -> 1}
-        # player -> 0/1 -> cnt
-        self.diagnals = {1: {0: 0, 1: 0}, 2: {0: 0, 1: 0}} # {1 -> 0 -> 2, 2 -> 0 -> 1}
-        """
-          0 1 2 3 4
-        0     x
-        1   x y
-        2 x
-        3
-        4
-
-          1 2
-        1 2 1
-        2   2
-
-          0 1 2
-        0 1   2
-        1   2
-        2     1
-        """
+        # player -> #row -> count
+        self.rows = { 1: defaultdict(int), 2: defaultdict(int) }
+        # player -> #col -> count
+        self.cols = { 1: defaultdict(int), 2: defaultdict(int) }
+        # player -> #diagonal (1 / 2) -> count
+        self.diagonals = { 1: defaultdict(int), 2: defaultdict(int) }
 
     def move(self, row: int, col: int, player: int) -> int:
+        """
+         0,1,2
+        0
+        1
+        2
+        """
         self.rows[player][row] += 1
         if self.rows[player][row] == self.n:
             return player
@@ -36,12 +26,13 @@ class TicTacToe:
             return player
 
         if row == col:
-            self.diagnals[player][0] += 1
-            if self.diagnals[player][0] == self.n:
+            self.diagonals[player][1] += 1
+            if self.diagonals[player][1] == self.n:
                 return player
+
         if row == self.n - col - 1:
-            self.diagnals[player][1] += 1
-            if self.diagnals[player][1] == self.n:
+            self.diagonals[player][2] += 1
+            if self.diagonals[player][2] == self.n:
                 return player
         
         return 0

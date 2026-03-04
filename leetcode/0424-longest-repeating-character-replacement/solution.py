9c753f1a73c4e812013c3f0e634ced0a6ced5b8b
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-04 19:16:42 +0000

    [LeetCode Sync] Runtime - 71 ms (94.69%), Memory - 19.7 MB (48.17%)

--- leetcode/0424-longest-repeating-character-replacement/solution.py
+++ leetcode/0424-longest-repeating-character-replacement/solution.py
@@ -1,19 +1,39 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        """
-        AABABBA
-        l
-         r
-        """
-        freq = defaultdict(int)
-        l = 0
-        best = 0
-        maxfreq = 0
-        for r, c in enumerate(s):
-            freq[c] += 1
-            maxfreq = max(freq[c], maxfreq)
-            while (r - l + 1) - maxfreq > k:
-                freq[s[l]] -= 1
-                l += 1
-            best = max(best, r - l + 1)
-        return best
+        # count = collections.Counter()
+        # if len(s) == 1:
+        #     return 1
+        # l = 0
+        # r = 0
+        # maxCnt = 0
+        # maxWin = 0
+
+        # for r in range(len(s)):
+        #     count[s[r]] += 1
+            
+        #     maxCnt = max(maxCnt, count[s[r]])
+        #     if r - l + 1 - maxCnt > k:
+        #             count[s[l]] -= 1
+        #             l += 1
+        #     maxWin = max(maxWin, r - l + 1)
+        # return maxWin
+        count = [0] * 26
+        # for i in range(len(s)):
+        #     idx = ord(s[i]) - ord('A')
+        #     count[idx] += 1
+        
+        maxCnt = 0
+        maxWin = 0
+        l = r = 0
+        for r in range(len(s)):
+            idx = ord(s[r]) - ord('A')
+            count[idx] += 1
+            maxCnt = max(maxCnt, count[idx])
+            if r - l + 1 - maxCnt > k:
+                lidx = ord(s[l]) - ord('A')
+                count[lidx] -= 1
+                l +=1
+            maxWin = max(maxWin, r - l + 1)
+        return maxWin
+
+

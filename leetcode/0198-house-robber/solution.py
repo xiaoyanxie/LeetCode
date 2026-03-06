13e7850e0fec8dbda95425d1644361a457fee1b2
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-06 00:23:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (86.03%)

--- leetcode/0198-house-robber/solution.py
+++ leetcode/0198-house-robber/solution.py
@@ -2,9 +2,7 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         dp = [0] * (len(nums) + 1)
         dp[1] = nums[0]
-        for i, n in enumerate(nums):
-            if i == 0: continue
-            j = i + 1
-            dp[j] = max(nums[i] + dp[j - 2], dp[j - 1])
-
-        return dp[-1]
+        for i in range(2, len(nums) + 1):
+            dp[i] = max(dp[i-1], dp[i - 2] + nums[i - 1])
+        
+        return dp[len(nums)]

3ff3173a63d3783b48039708fb6451a5140c32f8
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-08 20:48:45 +0000

    [LeetCode Sync] Runtime - 51 ms (16.52%), Memory - 53.5 MB (30.94%)

--- leetcode/0234-palindrome-linked-list/solution.py
+++ leetcode/0234-palindrome-linked-list/solution.py
@@ -5,38 +5,54 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # compute length
+        # 1. find the length
         length = 0
         tmp = head
         while tmp:
             length += 1
             tmp = tmp.next
-        
-        # reverse the first half
-        i = 0
+
+        # 2. reverse the first half
+        """
+        1
+
+        i = 2
+      <-3 <- 2    5 -> 2 -> 3
+             p.   c
+
+      <- 2   2
+         p.  c   
+        """
         prev, curr = None, head
+        i = 0
         while i < length // 2:
-            i += 1
             nxt = curr.next
             curr.next = prev
             prev, curr = curr, nxt
-        
-        # check palindrome
-        p1, p2 = prev, curr
+            i += 1
+
+        # 3. check palindrome
+        h1, h2 = prev, curr
         if length % 2 != 0:
-            p2 = p2.next
-        
+            h2 = h2.next
+
         isPal = True
-        while p1 and p2:
-            if p1.val != p2.val:
+        while h1 and h2:
+            if h1.val != h2.val:
                 isPal = False
                 break
-            p1, p2 = p1.next, p2.next
-        
-        # restore
+            h1, h2 = h1.next, h2.next
+
+        # 4. restore the first half
+        """
+          3 -> 2 -> 5 -> 2 -> 3
+     p    c
+        """
         while prev:
             nxt = prev.next
             prev.next = curr
             prev, curr = nxt, prev
-        
+
+        assert curr == head
+
         return isPal

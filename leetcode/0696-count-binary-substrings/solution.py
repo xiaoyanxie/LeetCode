5a1016093cafc234abf06d1f5e809213a1657011
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-08 06:33:40 +0000

    [LeetCode Sync] Runtime - 55 ms (61.02%), Memory - 19.5 MB (88.31%)

--- leetcode/0696-count-binary-substrings/solution.py
+++ leetcode/0696-count-binary-substrings/solution.py
@@ -1,51 +1,30 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
         """
-         zeros: 2
-         ones: 0
-         total: 1
+ prev   00222222
+ curr.  12121212
+        00110011
+        
+        total: 6
 
- ones    0     0     1    2   2   2    1   2
- zeros   1     2     2    2   1   2    2   2
-        "0     0     1    1   0   0    1   1"
-                                           ^
-                     1    1   1   1    1   1
- ones.  1 1 1 1 1
- zeros  0 1 1 1 1 
-        1 0 1 0 1
-          1 1 1 1
+ prev   011
+ curr   112
+        011
 
- ones   0 1
- zeros  1 1
-        0 1
-          1
-        """
-
-        """
-     1s  00122200
-     0s  12221200
-        "00110011"
-               ^
-         total = 4
-
-   prev  00222222
-   curr  12121212
-        "00110011"
-         
+        total: 1
         """
         prev = 0
         curr = 1
         total = 0
 
-        for i, c in enumerate(s):
-            if i == 0: 
-                continue
-            if s[i - 1] == c: # grouped
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]: # same group
                 curr += 1
-            else:
+            else: # different group
                 prev = curr
                 curr = 1
-            
+
             if prev >= curr:
                 total += 1
+        
         return total

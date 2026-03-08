58288c4e323cb9bcfb961f44c13f7e67e4760880
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-08 19:26:34 +0000

    [LeetCode Sync] Runtime - 27 ms (44.34%), Memory - 27.6 MB (26.98%)

--- leetcode/0238-product-of-array-except-self/solution.py
+++ leetcode/0238-product-of-array-except-self/solution.py
@@ -1,20 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # scan the array, create a total product
-        total = 1
-        zeroIdx = -1
-        for i, num in enumerate(nums):
-            # if more than one zero
-            if num == 0:
-                if zeroIdx != -1: return [0] * len(nums)
-                else: zeroIdx = i
-            else:
-                total *= num
-        
-        if zeroIdx != -1:
-            products = [0] * len(nums)
-            products[zeroIdx] = total
-            return products
+        #nums = [1234]
+        #prefix product = [1,2,6,24]
+        #idx: 1,12,123,1234^2^3^4
+        #nums[-1] = [4321]
+        #suffix product = [4,12,24,1]
+                        #    ^3^2^1
+                       #   idx: 4,43,432,4321
+        prefix = []
+        suffix = []
+        product = 1
+        n = len(nums)
+        for num in nums:
+            product *= num
+            prefix.append(product)
+        product = 1
+        for i in range(n):
+            product *= nums[n-1-i]
+            suffix.append(product)
 
-        # if less than two zeros
-        return [ int(total / num) for num in nums ]
+        res = [suffix[n -2]]
+        for i in range(0, n - 2):
+            res.append(prefix[i] * suffix[n - 3 - i])
+        
+        res.append(prefix[-2])
+        return res

5ec80f1c0f572ee388421fd3c87a00c827a36cf5
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-09 05:55:09 +0000

    [LeetCode Sync] Runtime - 7 ms (96.47%), Memory - 19.9 MB (51.76%)

--- leetcode/1272-invalid-transactions/solution.py
+++ leetcode/1272-invalid-transactions/solution.py
@@ -1,44 +1,66 @@
+from collections import deque, defaultdict
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         """
-         txn1, txn2, txn3, ....
-        | ------ 60 min ------ |
+        txn invalid if:
+           amt > 1000
+           txn.name occurs, txn.city different
+
+        window: [ (name, time, city, idx) ]
+        mapping: {
+            name -> { (idx, city1), (idx, city2) }
+        }
+
+        "alice,20,800,mtv","alice,50,100,beijing"
+
+        txns: [ (alice, 20, 800, mtv, 0), (alice, 80, 100, beijing, 1) ]
+        window: (alice, 20, mtv, 0)
+        mapping: {
+            alice: { (0, mtv) }
+        }
+
+
+        "alice,20,800,mtv","alice,50,1200,mtv"
         
-        while abs(txn1.time - new.time) > 60:
-            queue.popleft()
-        
-        if new.amt > 1000:
-            invalid.add(i)
-        
-        for txn in queue:
-            if txn.name == new.name and txn.city != new.city:
-                invalid.add(i)
-                invalid.add(txn.i)
-        
+        txns: [ (alice, 20, 800, mtv, 0), (alice, 50, 1200, mtv, 1) ]
+        window: (alice, 20, mtv, 0)
+        mapping: {
+            alice: { (0, mtv) }
+        }
         """
-
-        def parse(txn):
+        txns = []
+        for i, txn in enumerate(transactions):
             name, time, amt, city = txn.split(',')
-            return (name, int(time), int(amt), city, txn)
+            txns.append((name, int(time), int(amt), city, i))
 
-        txns = [ parse(txn) for txn in transactions ]
-        txns.sort(key=lambda x: x[1])
+        txns.sort(key=lambda txn: txn[1])
+
         invalid = set()
-        queue = deque()
-        for i, txn in enumerate(txns):
-            name, time, amt, city, _ = txn
-            
-            while queue and abs(time - queue[0][2]) > 60:
-                queue.popleft()
-            
+        window = deque() # 60 min
+        mapping = defaultdict(set)
+        for name, time, amt, city, idx in txns: # alice, 50, 100, beijing, 1
+            # print(f'checking: {name}, {time}, {amt}, {city}, {idx}')
             if amt > 1000:
-                invalid.add(i)
+                # print(f'invalid: {name} has amt={amt} > 1000')
+                invalid.add(idx)
+
+            while window and time - window[0][1] > 60:
+                lname, _, lcity, lidx = window.popleft()
+                assert lname in mapping and (lidx, lcity) in mapping[lname]
+                mapping[lname].remove((lidx, lcity))
+                if not mapping[lname]:
+                    del mapping[lname]
             
-            for txn in queue:
-                if txn[1] == name and txn[3] != city:
-                    invalid.add(txn[0])
-                    invalid.add(i)
+            # print(f'window={window}, mapping={mapping}')
+            if name in mapping:
+                for otherIdx, otherCity in mapping[name]:
+                    # same name, different city
+                    if city != otherCity:
+                        # print(f'invalid: {name} happened in both {city} and {otherCity}')
+                        invalid.add(otherIdx)
+                        invalid.add(idx)
             
-            queue.append((i, name, time, city))
+            window.append((name, time, city, idx))
+            mapping[name].add((idx, city))
         
-        return [ txns[i][4] for i in invalid ]
+        return [ transactions[i] for i in invalid ]

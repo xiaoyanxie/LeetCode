d4d0c3300eb100441edbd26c827b5c1076ca0be8
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-09 02:01:14 +0000

    [LeetCode Sync] Runtime - 3915 ms (52.26%), Memory - 19.6 MB (56.73%)

--- leetcode/0079-word-search/solution.py
+++ leetcode/0079-word-search/solution.py
@@ -1,34 +1,50 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        """
+        dfs(i, j, k, visited)    
+        
+        ABCCED
+            k
+
+        dfs(0, 0, 0, {A}) -> dfs(1, 0, 1, {A, S}) -> False
+                          -> dfs(0, 1, 1, {A, B}) -> dfs(0, 2, 2, {A, B, C}) -> dfs(0, 3, 3, {A, B, C, E}) -> False
+                                                                             -> dfs(1, 2, 3, {A, B, C, C}) -> ... -> dfs(2, 1, 5, {A, B, C, C, E, D}) -> True
+        """
         rows = len(board)
         cols = len(board[0])
+
         directions = [ (0, 1), (0, -1), (1, 0), (-1, 0) ]
 
-        def isValidLoc(i, j):
+        def isValidLoc(i, j) -> bool:
             return 0 <= i < rows and 0 <= j < cols
 
-        def match(i, j, wi):
-            if wi == len(word):
+        def dfs(i, j, k) -> bool: # k=5, i=2, j=1
+            # base case:
+            if k == len(word) - 1:
                 return True
-            if board[i][j] != word[wi]:
-                return False
-            if wi == len(word) - 1:
-                return True
-
+            
             for di, dj in directions:
                 ni, nj = i + di, j + dj
-                if not isValidLoc(ni, nj) or board[ni][nj] == '':
+                if not isValidLoc(ni, nj) or board[ni][nj] == '' or board[ni][nj] != word[k + 1]:
                     continue
-                tmp, board[i][j] = board[i][j], ''
-                if match(ni, nj, wi + 1):
-                    return True
-                board[i][j] = tmp
-            return False
+                
+                char = board[ni][nj]
+                board[ni][nj] = ''
 
+                if dfs(ni, nj, k + 1):
+                    board[ni][nj] = char
+                    return True
+                board[ni][nj] = char
+            
+            return False
+        
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] != word[0]:
-                    continue
-                if match(i, j, 0):
-                    return True
+                if board[i][j] == word[0]:
+                    board[i][j] = ''
+                    if dfs(i, j, 0):
+                        board[i][j] = word[0]
+                        return True
+                    board[i][j] = word[0]
+        
         return False

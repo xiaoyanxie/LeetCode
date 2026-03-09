8a79c91a68324e1024dab778e614760553539163
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-09 00:40:29 +0000

    [LeetCode Sync] Runtime - 37 ms (56.25%), Memory - 21.9 MB (84.03%)

--- leetcode/1582-design-browser-history/solution.py
+++ leetcode/1582-design-browser-history/solution.py
@@ -5,32 +5,19 @@ class BrowserHistory:
         self.index = 0
 
     def visit(self, url: str) -> None:
-        """
-        ["leetcode.com", "google.com", "facebook.com", "linkedin.com"]
-               i
-        """
-        while len(self.history) > self.index + 1:
+        while self.index < len(self.history) - 1:
             self.history.pop()
         
-        assert self.index == len(self.history) - 1
-        assert len(self.history) > 0
         self.history.append(url)
         self.index += 1
 
     def back(self, steps: int) -> str:
-        if self.index - steps >= 0:
-            self.index -= steps
-        else:
-            self.index = 0
+        self.index = max(self.index - steps, 0)
         return self.history[self.index]
 
     def forward(self, steps: int) -> str:
-        if self.index + steps < len(self.history):
-            self.index += steps
-        else:
-            self.index = len(self.history) - 1
+        self.index = min(self.index + steps, len(self.history) - 1)
         return self.history[self.index]
-
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)

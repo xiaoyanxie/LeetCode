09bdf143b8249e32d7876e52eb0f36054a0e1612
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-09 06:34:18 +0000

    [LeetCode Sync] Runtime - 62 ms (7.46%), Memory - 50.4 MB (17.11%)

--- leetcode/2169-simple-bank-system/solution.py
+++ leetcode/2169-simple-bank-system/solution.py
@@ -1,31 +1,44 @@
 class Bank:
 
     def __init__(self, balance: List[int]):
-        self.accounts = list(balance)
+        self.accounts = [0] * (len(balance) + 1)
+        for i, b in enumerate(balance):
+            self.accounts[i + 1] = b
+        self.n = len(balance)
+
+        """
+        accounts: {
+            0: 0,
+            1: 10,
+            2: 100,
+            3: 10,
+            4: 50,
+            5: 30
+        }
+        """
+
+    def isValidAccNo(self, accNo):
+        return 1 <= accNo <= self.n
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if not self.withdraw(account1, money):
+        if not self.isValidAccNo(account1) or not self.isValidAccNo(account2) or self.accounts[account1] < money:
             return False
-        if not self.deposit(account2, money):
-            self.deposit(account1, money)
-            return False
-        return True
         
-    def __isValidAcc(self, accNo):
-        return 1 <= accNo <= len(self.accounts)
+        self.accounts[account1] -= money
+        self.accounts[account2] += money
+        return True
 
     def deposit(self, account: int, money: int) -> bool:
-        if not self.__isValidAcc(account):
+        if not self.isValidAccNo(account):
             return False
-        self.accounts[account - 1] += money
+        self.accounts[account] += money
         return True
 
     def withdraw(self, account: int, money: int) -> bool:
-        if not self.__isValidAcc(account):
+        if not self.isValidAccNo(account) or self.accounts[account] < money:
             return False
-        if self.accounts[account - 1] < money:
-            return False
-        self.accounts[account - 1] -= money
+        
+        self.accounts[account] -= money
         return True
 
 

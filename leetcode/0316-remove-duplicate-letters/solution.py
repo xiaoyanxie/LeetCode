01d9ea492e7ba71ca865ffa0ba3d0b56f9bf1d4c
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-10 19:33:18 +0000

    [LeetCode Sync] Runtime - 3 ms (64.92%), Memory - 19.5 MB (47.91%)

--- leetcode/0316-remove-duplicate-letters/solution.py
+++ leetcode/0316-remove-duplicate-letters/solution.py
@@ -2,32 +2,41 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         """
         cbacdcbc
+
+        cdacdcbc
                 ^
         acdb
+        
+        seen: set
+        stack
+        freq: hashmap
 
-        cbaxycbxy
+        1. while s[i] not in seen and s[i] < acc[-1] and acc[-1] has duplicates
+            then remove acc[-1]
+        2. concat s[i]
+
+        freq: {c:0, d:0, a:0, b:0}
+        stack: acdb
+        seen: acdb
+        cdacdcbc
                 ^
-        stack: a,c,b,x,y
-        seen: {a,c,b,x,y}
-        freq: {c:0, b:0, a:0, x:0, y:0}
-
-        bcabc
-            ^
-        abc
         """
-        stack = []
+
         seen = set()
+        stack = []
         freq = Counter(s)
+
         for c in s:
             freq[c] -= 1
 
             if c in seen:
                 continue
-            
+
             while stack and c < stack[-1] and freq[stack[-1]] > 0:
                 seen.remove(stack.pop())
             
             stack.append(c)
             seen.add(c)
         
+        # print(f'freq={freq}, stack={stack}')
         return ''.join(stack)

c837f9ab8b89c63e9fbb613e5a70660cb887bdba
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-10 04:55:48 +0000

    [LeetCode Sync] Runtime - 453 ms (62.91%), Memory - 106.6 MB (39.22%)

--- leetcode/3678-design-task-manager/solution.py
+++ leetcode/3678-design-task-manager/solution.py
@@ -1,39 +1,33 @@
-from collections import defaultdict
-import heapq
 class TaskManager:
 
     def __init__(self, tasks: List[List[int]]):
-        """
-        userId, taskId, priority
-        """
-
-        # order by priority, taskId DESC
-        self.queue = []
-        self.index = defaultdict(Tuple[int, int])
-        for userId, taskId, priority in tasks:
-            self.add(userId, taskId, priority)
+        self.mp = {}
+        self.heap = []
+        for uid, tid, p in tasks:
+            self.mp[tid] = [uid, p]
+            self.heap.append((-p, -tid))
+        heapify(self.heap)
 
     def add(self, userId: int, taskId: int, priority: int) -> None:
-        heapq.heappush(self.queue, (-priority, -taskId, userId))
-        self.index[taskId] = (userId, priority)
+        self.mp[taskId] = [userId, priority]
+        heappush(self.heap, (-priority, -taskId))
 
     def edit(self, taskId: int, newPriority: int) -> None:
-        userId, _ = self.index[taskId]
-        self.add(userId, taskId, newPriority)
+        self.mp[taskId][1] = newPriority
+        heappush(self.heap, (-newPriority, -taskId))
 
     def rmv(self, taskId: int) -> None:
-        if taskId in self.index:
-            del self.index[taskId]
+        del self.mp[taskId]
 
     def execTop(self) -> int:
-        while self.queue:
-            priority, taskId, userId = heapq.heappop(self.queue)
-            priority, taskId = abs(priority), abs(taskId)
-            if taskId in self.index and self.index[taskId] == (userId, priority):
-                self.rmv(taskId)
-                return userId
+        while self.heap:
+            p, tid = heappop(self.heap)
+            p, tid = -p, -tid
+            if tid in self.mp and self.mp[tid][1] == p:
+                uid = self.mp[tid][0]
+                del self.mp[tid]
+                return uid
         return -1
-
 
 # Your TaskManager object will be instantiated and called as such:
 # obj = TaskManager(tasks)

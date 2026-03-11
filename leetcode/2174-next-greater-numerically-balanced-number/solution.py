8cfeae7c7cffbf740da72012f0ca56389d6e5c52
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-11 22:31:34 +0000

    [LeetCode Sync] Runtime - 68 ms (72.86%), Memory - 19.4 MB (74.50%)

--- leetcode/2174-next-greater-numerically-balanced-number/solution.py
+++ leetcode/2174-next-greater-numerically-balanced-number/solution.py
@@ -1,75 +1,40 @@
 class Solution:
     def nextBeautifulNumber(self, n: int) -> int:
-        """
-        10,11,12,13,14,15,16,17,......20,21,22
+        if n == 0:
+            return 1
 
-        1 digit : 1
-        2 digits: 22
-        3 digits: 122, 221, 333
-        4 digits: 1333, 3331, 4444
-        5 digits: 14444, 22333, 33322, 44441, 55555
+        def numDigits():
+            digits = 0
+            m = n
+            while m > 0:
+                digits += 1
+                m //= 10
+            return digits
 
-
-
-        123: 123 - 1230
-        """
-
-        combinations = [
-            [1],
-            [2,2],
-            [1,2,2],[3,3,3],
-            [1,3,3,3],[4,4,4,4],
-            [1,4,4,4,4],[2,2,3,3,3],[5,5,5,5,5],
-            [1,2,2,3,3,3],[4,4,4,4,2,2],[5,5,5,5,5,1],[6,6,6,6,6,6],
-            [1,2,2,4,4,4,4]
-        ]
-
-        nums = []
-        for combo in combinations:
-            for p in set(itertools.permutations(combo)):
-                num = int(''.join(map(str, p)))
-                nums.append(num)
-
-        nums.sort()
-
-        for num in nums:
-            if num > n:
-                return num
-
-        # def isBalanced(x):
-        #     xs = str(x)
-        #     cnt = Counter(xs)
-        #     for d in cnt:
-        #        if cnt[d] != int(d):
-        #          return False
-        #     return True
-
-        # def search(l, r):
-        #     if l < r:
-        #         return None
-        #     mid = l + (r - l) // 2
-        #     if isBalanced(mid):
-        #         return mid
-        #     left = search(l, mid - 1)
-        #     if left:
-        #         return left
-        #     return search(mid + 1, r)
+        def generate(digits):
+            if digits == 0:
+                return []
+            num = str(digits) * digits
+            numbers = [num]
+            for k in range(digits // 2 + 1):
+                left = str(digits - k) * (digits - k)
+                for right in generate(k):
+                    if left == right:
+                        continue
+                    numbers.append(left + right)
+            return numbers
         
-        # return search()
-        # def maxAt(x):
-        #     s = str(x)
-        #     return int(str(len(s)) * len(s))
+        groups = { i: generate(i) for i in range(1, 8) }
 
-        # def nextN(x):
-        #     s = str(x)
-        #     return 10**(len(s))
+        digits = numDigits()
+        if n >= int(str(digits) * digits):
+            digits += 1
+        
+        possibleNumbers = set()
+        for nums in groups[digits]:
+            for candidate in itertools.permutations(nums):
+                possibleNumbers.add(int(''.join(candidate)))
 
-        # n += 1
-        # maxN = n * 100
-        # while n < maxN:
-        #     if isBalanced(n):
-        #         return n
-        #     if n > maxAt(n):
-        #         n = nextN(n)
-        #     else:
-        #         n += 1
+        numbers = sorted(list(possibleNumbers))
+        i = bisect.bisect_right(numbers, n)
+        return numbers[i]

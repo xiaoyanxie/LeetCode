4fe2d31d12db797a6bcc2178b7daaa8c0a2ce012
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-11 23:54:57 +0000

    [LeetCode Sync] Runtime - 113 ms (71.02%), Memory - 76.8 MB (80.05%)

--- leetcode/0146-lru-cache/solution.py
+++ leetcode/0146-lru-cache/solution.py
@@ -1,19 +1,37 @@
 class Node:
     def __init__(self, key=None, val=None):
-        self.key=key
-        self.val=val
-        self.prev=None
-        self.next=None
-
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+    
     def unlink(self):
         self.prev.next = self.next
         self.next.prev = self.prev
-        self.prev = None
         self.next = None
+        self.prev = None
+    
+    def insertLeft(self, node):
+        self.prev.next = node
+        node.prev = self.prev
+
+        self.prev = node
+        node.next = self
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        """
+        "LRUCache", "put", "put",   "get", "put",  "get", "put", "get", "get", "get"
+        [2],        [1, 1], [2, 2], [1],   [3, 3], [2],   [4, 4], [1],  [3],   [4]
+
+        size = 2
+        cache: {
+            1: node(1, 1),
+            2: node(2, 2)
+        }
+        head <-> node(1, 1) <-> tail
+        """
         self.capacity = capacity
         self.cache = {}
         self.head = Node()
@@ -21,22 +39,13 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def moveToTail(self, node):
-        prev = self.tail.prev
-
-        self.tail.prev = node
-        node.next = self.tail
-
-        prev.next = node
-        node.prev = prev
-
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         
         node = self.cache[key]
         node.unlink()
-        self.moveToTail(node)
+        self.tail.insertLeft(node)
         return node.val
 
     def put(self, key: int, value: int) -> None:
@@ -44,18 +53,18 @@ class LRUCache:
             node = self.cache[key]
             node.val = value
             node.unlink()
-            self.moveToTail(node)
-            return node.val
-        
+            self.tail.insertLeft(node)
+            return
+
         if len(self.cache) == self.capacity:
+            # evcit
             evcit = self.head.next
             evcit.unlink()
             del self.cache[evcit.key]
-        
+
         node = Node(key, value)
         self.cache[key] = node
-        self.moveToTail(node)
-
+        self.tail.insertLeft(node)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)

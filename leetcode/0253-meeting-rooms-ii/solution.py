42b7251a3cda61fc8848aa27d1a2ffd26f271b0e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-12 20:31:05 +0000

    [LeetCode Sync] Runtime - 5 ms (33.05%), Memory - 21.3 MB (90.89%)

--- leetcode/0253-meeting-rooms-ii/solution.py
+++ leetcode/0253-meeting-rooms-ii/solution.py
@@ -1,21 +1,11 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        """
-        [[5,10],[15,20],[0,30]]
-                    i
-                    j
-        [[5,10],[7,8],[9,11]]
-
-        [[2,11],[6,16],[11,16]]
-                  i   
-                         j
-        """
         intervals.sort()
-        heap = []
-        for i in range(len(intervals)):
-            starti, endi = intervals[i]
-            if heap and starti >= heap[0]:
-                heapq.heappop(heap)
-            heapq.heappush(heap, endi)
-
-        return len(heap)
+        earliestEndTimes = []
+        
+        for start, end in intervals:
+            if earliestEndTimes and earliestEndTimes[0] <= start:
+                heapq.heappop(earliestEndTimes)
+            heapq.heappush(earliestEndTimes, end)
+        
+        return len(earliestEndTimes)

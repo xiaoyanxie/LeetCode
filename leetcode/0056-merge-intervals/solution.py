411d5c285b30f5f5104c0d1e4d1e3c2cb70b7ff2
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-12 20:38:00 +0000

    [LeetCode Sync] Runtime - 7 ms (71.02%), Memory - 22.4 MB (82.40%)

--- leetcode/0056-merge-intervals/solution.py
+++ leetcode/0056-merge-intervals/solution.py
@@ -1,16 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
         """
-        [1,3],[2,6],[8,10],[15,18]
+        [1,  3],
+          [2,    6],
+                     [8,        10], [15,       18]
+
+        [1,  3],
+            [3,   6],
+                     [8,        10], [15,       18]
+        
+        [1,  3],
+            [3,   6],       [9, 10]
+                     [8,           11], 
         """
+
         intervals.sort()
         ret = [intervals[0]]
-        for i, interval in enumerate(intervals):
-            if i == 0: continue
-            start, end = interval
-            if start <= ret[-1][1]:
-                ret[-1][0] = min(start, ret[-1][0])
-                ret[-1][1] = max(end, ret[-1][1])
+        for i in range(1, len(intervals)):
+            if ret[-1][1] >= intervals[i][0]:
+                ret[-1][1] = max(ret[-1][1], intervals[i][1])
             else:
-                ret.append(interval)
+                ret.append(intervals[i])
         return ret

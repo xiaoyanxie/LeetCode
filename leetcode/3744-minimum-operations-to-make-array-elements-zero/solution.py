f03563159d46d435278137b23eb5af68b0c9cdb5
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-12 18:44:00 +0000

    [LeetCode Sync] Runtime - 1439 ms (37.01%), Memory - 58.1 MB (35.04%)

--- leetcode/3744-minimum-operations-to-make-array-elements-zero/solution.py
+++ leetcode/3744-minimum-operations-to-make-array-elements-zero/solution.py
@@ -1,90 +1,55 @@
-import heapq
-
-class Bucket:
-    def __init__(self,size,ops):
-        self.size = size
-        self.ops = ops
-
 class Solution:
     def minOperations(self, queries: List[List[int]]) -> int:
         """
-        40,[41,42],[43,44],[45,46],[47,48]
+        nums = [n, n + 1, n + 2, ..., n + m]
 
-        48 -> 12 -> 3 -> 0
-        46 -> 11 -> 2 -> 0
-        44 -> 11 -> 2 -> 0
-        42 -> 10 -> 2 -> 0
+        nums[i] // 2
+
+        log4(nums[i])
+
+
         """
-        def simulate(start, end):
-            maxheap = []
-            cnt = 0
-            for n in range(start, end + 1):
-                heapq.heappush(maxheap, -n)
-            
-            while len(maxheap) >= 2:
-                a = -heapq.heappop(maxheap)
-                b = -heapq.heappop(maxheap)
-                a, b = math.floor(a / 4), math.floor(b / 4)
-                cnt += 1
-                if a > 0:
-                    heapq.heappush(maxheap, -a)
-                if b > 0:
-                    heapq.heappush(maxheap, -b)
-            if maxheap:
-                cnt += (math.floor(math.log(-maxheap[0], 4)) + 1)
-            return cnt
+        def steps(n):
+            return math.floor(math.log(n, 4)) + 1
 
-        def splitBuckets(start, end):
-            opsLo = math.floor(math.log(start, 4)) + 1
-            opsHi = math.floor(math.log(end, 4)) + 1
-            buckets = []
-            for k in range(opsLo, opsHi + 1):
-                startk = start if k == opsLo else 4**(k - 1)
-                endk = end if k == opsHi else 4**k - 1
-                buckets.append([endk - startk + 1, k])
-            return buckets
-
-        def count(start, end):
-            """
-            [4^0      ,4^1 - 1] -> 1
-            [4^1      ,4^2 - 1] -> 2
-            [4^2      ,4^3 - 1] -> 3
-            [4^3      ,4^4 - 1] -> 4
-            [4^(k - 1),4^k - 1] -> k
-            
-            Example:
-            [4,63] = [4,15] + [16,63]
-            [a,b] = [bucket1] + [bucket2] + [bucket3] + ... + [bucketN]
-
-            Pseudo-code for each bucket:
-            queryi = [a, b]
-            buckets = splitBuckets(queryi) # ordered asc
-
+        def numOps(l, r) -> int:
+            heap = [ (-steps(n), n) for n in range(l, r + 1) ]
+            heapq.heapify(heap)
+            # print(f'heap={heap}')
             ops = 0
-            largest = buckets.pop()
-            while buckets:
-                nxt = buckets.pop()
-                ops += math.ceil(largest.size * (largest.ops - nxt.ops) / 2)
-                nxt.size += largest.size
-                largest = nxt
+            while len(heap) >= 2:
+                _, n1 = heapq.heappop(heap)
+                _, n2 = heapq.heappop(heap)
+                # print(f'choose ({n1}, {n2}) in {[n for stps, n in heap]}')
+                n1 //= 4
+                n2 //= 4
+                if n1 > 0:
+                    heapq.heappush(heap, (-steps(n1), n1))
+                if n2 > 0:
+                    heapq.heappush(heap, (-steps(n2), n2))
+                ops += 1
             
-            ops += math.ceil(largest.size * largest.ops / 2)
-            """
-            buckets = splitBuckets(start, end)
-            # print(buckets)
-            ops = 0
-            largest = buckets.pop()
-            while buckets:
-                nxt = buckets.pop()
-                ops += largest[0] * (largest[1] - nxt[1]) / 2
-                nxt[0] += largest[0]
-                largest = nxt
+            if heap:
+                stps, _ = heap.pop()
+                ops += abs(stps)
+            print(f'{r - l + 1} elements takes {ops} ops')
+            return ops
 
-            ops += largest[0] * largest[1] / 2
-            return math.ceil(ops)
+        def prefixSteps(n):
+            # return sum(steps(n) for n in range(1, n + 1))
+            steps = 0
+            p = 1
+            k = 1
 
-        cnt = 0
-        for start, end in queries:
-            # cnt += simulate(start, end)
-            cnt += count(start, end)
-        return cnt
+            while p <= n:
+                steps += k * ((min(p * 4 - 1, n) - p) + 1)
+                p *= 4
+                k += 1
+
+            return steps
+            
+        total = 0
+        for l, r in queries:
+            # print(prefixSteps(r), prefixSteps(l - 1))
+            total += math.ceil((prefixSteps(r) - prefixSteps(l - 1)) / 2)
+        return total

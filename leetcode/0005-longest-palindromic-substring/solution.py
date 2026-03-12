58830f888ae247984b7fb1166fe49cb8aabbc7f8
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-12 19:22:52 +0000

    [LeetCode Sync] Runtime - 263 ms (59.37%), Memory - 19.3 MB (89.44%)

--- leetcode/0005-longest-palindromic-substring/solution.py
+++ leetcode/0005-longest-palindromic-substring/solution.py
@@ -1,20 +1,39 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        """
+        babad
+
+        cbbd
+        """
+
         def expand(i, j) -> str:
-            if s[j] != s[i]:
-                return ''
-            while 0 <= i and j < len(s) and s[i] == s[j]:
+            """
+            aabaa
+            i
+                 j
+            
+            cbbd
+             i
+               j
+
+            a
+            i
+             j
+            """
+            while 0 <= i and j < len(s):
+                if s[i] != s[j]:
+                    break
                 i -= 1
                 j += 1
-            return s[i + 1: j]
-
-        best = ''
+            return s[i + 1:j]
+        
+        best = s[0]
         for i in range(len(s)):
-            s1 = expand(i, i)
-            if len(s1) > len(best):
-                best = s1
-            if i > 0:
-                s2 = expand(i - 1, i)
-                if len(s2) > len(best):
-                    best = s2
+            p1 = expand(i, i)
+            if len(p1) > len(best):
+                best = p1
+
+            p2 = expand(i, i + 1)
+            if len(p2) > len(best):
+                best = p2
         return best

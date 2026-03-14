db3891b4a282c88e881169c9cee6fdbeb4cd10e4
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-14 19:41:37 +0000

    [LeetCode Sync] Runtime - 71 ms (16.75%), Memory - 19.5 MB (79.17%)

--- leetcode/0723-candy-crush/solution.py
+++ leetcode/0723-candy-crush/solution.py
@@ -1,69 +1,58 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        rows = len(board)
-        cols = len(board[0])
-        vertical = [ (1, 0), (-1, 0) ]
-        horizontal = [ (0, 1), (0, -1) ]
+        m = len(board)
+        n = len(board[0])
 
-        def isValidLoc(i, j):
-            return 0 <= i < rows and 0 <= j < cols
+        def crash() -> bool:
+            stable = True
+            for i in range(m):
+                for j in range(n - 2):
+                    v1, v2, v3 = abs(board[i][j]), abs(board[i][j + 1]), abs(board[i][j + 2])
+                    if v1 != 0 and v1 == v2 == v3:
+                        board[i][j] = board[i][j + 1] = board[i][j + 2] = -v1
+                        stable = False
+        
+            for j in range(n):
+                for i in range(m - 2):
+                    v1, v2, v3 = abs(board[i][j]), abs(board[i + 1][j]), abs(board[i + 2][j])
+                    if v1 != 0 and v1 == v2 == v3:
+                        board[i][j] = board[i + 1][j] = board[i + 2][j] = -v1
+                        stable = False
+                    
+            if not stable:
+                for i in range(m):
+                    for j in range(n):
+                        if board[i][j] < 0:
+                            board[i][j] = 0
+            return stable
 
-        def detectCrash(i, j, direction, candidates):
-            potentials = []
-            if direction == vertical:
-                for ni in reversed(range(i - 2, i)):
-                    if not isValidLoc(ni, j) or board[ni][j] != board[i][j]:
-                        break
-                    potentials.append((ni, j))
-                for ni in range(i, i + 2):
-                    if not isValidLoc(ni, j) or board[ni][j] != board[i][j]:
-                        break
-                    potentials.append((ni, j))
-            else:
-                for nj in reversed(range(j - 2, j)):
-                    if not isValidLoc(i, nj) or board[i][nj] != board[i][j]:
-                        break
-                    potentials.append((i, nj))
-                for nj in range(j, j + 2):
-                    if not isValidLoc(i, nj) or board[i][nj] != board[i][j]:
-                        break
-                    potentials.append((i, nj))
-
-            if len(potentials) >= 3:
-                for ni, nj in potentials:
-                    candidates.add((ni, nj))
-
-        # def crash(candidates):
-        #     for i, j in candidates:
-        #         board[i][j] = 0
-
-        def gravityPull(j):
-            tmp = [ board[i][j] for i in range(rows) if board[i][j] != 0 ]
-            if len(tmp) == rows:
-                return
+        def drop(j):
+            """t
+            0  
+            0   
+            0  
+            0 b
+            1 
+            3  
+            2
+            1  
+            """
+            t = m - 1
+            while t >= 0 and board[t][j] != 0:
+                t -= 1
             
-            zeros = rows - len(tmp)
-            for i in range(0, zeros):
-                board[i][j] = 0
-            for i in range(zeros, rows):
-                board[i][j] = tmp[i - zeros]
-
-        while True:
-            # scan the board 
-            candidates = set()
-            for i in range(rows):
-                for j in range(cols):
-                    if board[i][j] == 0:
-                        continue
-                    detectCrash(i, j, vertical, candidates)
-                    detectCrash(i, j, horizontal, candidates)
-            
-            if not candidates:
-                break
-
-            for i, j in candidates:
-                board[i][j] = 0
-            
-            for j in range(cols):
-                gravityPull(j)
+            b = t
+            while t >= 0:
+                if board[t][j] != 0:
+                    board[b][j], board[t][j] = board[t][j], board[b][j]
+                    b -= 1
+                t -= 1
+        
+        stable = False
+        while not stable:
+            stable = crash()
+            if stable: break
+            for j in range(n):
+                drop(j)
         return board
+

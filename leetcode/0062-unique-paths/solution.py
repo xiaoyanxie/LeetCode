f2ec74779bb2f7242845fd42c67499a6c6019317
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-14 02:02:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (90.66%)

--- leetcode/0062-unique-paths/solution.py
+++ leetcode/0062-unique-paths/solution.py
@@ -1,23 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        """
-        Paths[i][j] = Paths[i - 1][j] + Paths[i][j - 1]
-        Paths[0][0] = 1
-
-        1,1,1,1,1,1,1
-        1,2,3,4,5,6,7
-        1,3,6,10,15,21,28
-
-        """
-
-        paths = [ [0] * n for _ in range(m) ]
-        paths[0][0] = 1
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    continue
-                top = paths[i - 1][j] if i > 0 else 0
-                left = paths[i][j - 1] if j > 0 else 0
-                paths[i][j] =  top + left
-
-        return paths[m - 1][n - 1]
+        #subproblem: for the dp[i], robot can come from two dirctions, 
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0]
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1]
+            
+        for i in range(1, m):
+            for j in range(1, n):
+                if dp[i][j] == 0:
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        
+        return dp[m - 1][n - 1]

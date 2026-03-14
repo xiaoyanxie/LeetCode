5b56e24aded5cd9e4a4961a1abdafd56b8c4a758
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-14 19:57:06 +0000

    [LeetCode Sync] Runtime - 273 ms (17.36%), Memory - 26.8 MB (24.86%)

--- leetcode/0200-number-of-islands/solution.py
+++ leetcode/0200-number-of-islands/solution.py
@@ -1,30 +1,42 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+        visited = set()
+        islands = 0
+        m = len(grid)
+        n = len(grid[0])
         directions = [ (0, 1), (0, -1), (1, 0), (-1, 0) ]
-        # visited = set()
 
-        def isValidLoc(i, j):
-            return 0 <= i < rows and 0 <= j < cols
+        def isValidLoc(i, j) -> bool:
+            return 0 <= i < m and 0 <= j < n
+
+        """
+        visited: (0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1), (0, 2), (0, 3), (1, 3)
+
+            0   1   2   3   4
+        0 ["1","1","1","1","0"],
+        1 ["1","1","0","1","0"],
+        2 ["1","1","0","0","0"],
+        3 ["0","0","0","0","0"]
+        
+        """
 
         def visit(i, j):
-            stack = [(i, j)]
+            visited.add((i, j))
+            stack = [(i, j)] # (1, 3)
             while stack:
                 i, j = stack.pop()
-                # visited.add((i, j))
-                grid[i][j] = '2'
                 for di, dj in directions:
                     ni, nj = i + di, j + dj
-                    if not isValidLoc(ni, nj) or grid[ni][nj] != '1': # or (ni, nj) in visited:
+                    if not isValidLoc(ni, nj) or grid[ni][nj] == '0' or (ni, nj) in visited:
                         continue
                     stack.append((ni, nj))
+                    visited.add(stack[-1])
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    visit(i, j)
+                    islands += 1
         
-        islands = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] != '1': # or (i, j) in visited:
-                    continue
-                islands += 1
-                visit(i, j)
         return islands
+

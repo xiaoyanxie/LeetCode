678d244ae067a515254ed1902d5f4f5d04cdbf57
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-15 18:09:47 +0000

    [LeetCode Sync] Runtime - 258 ms (32.92%), Memory - 21.6 MB (67.67%)

--- leetcode/0200-number-of-islands/solution.py
+++ leetcode/0200-number-of-islands/solution.py
@@ -1,42 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        islands = 0
         m = len(grid)
         n = len(grid[0])
-        directions = [ (0, 1), (0, -1), (1, 0), (-1, 0) ]
+        count = 0
 
-        def isValidLoc(i, j) -> bool:
-            return 0 <= i < m and 0 <= j < n
-
-        """
-        visited: (0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1), (0, 2), (0, 3), (1, 3)
-
-            0   1   2   3   4
-        0 ["1","1","1","1","0"],
-        1 ["1","1","0","1","0"],
-        2 ["1","1","0","0","0"],
-        3 ["0","0","0","0","0"]
-        
-        """
-
-        def visit(i, j):
-            visited.add((i, j))
-            stack = [(i, j)] # (1, 3)
-            while stack:
-                i, j = stack.pop()
-                for di, dj in directions:
-                    ni, nj = i + di, j + dj
-                    if not isValidLoc(ni, nj) or grid[ni][nj] == '0' or (ni, nj) in visited:
-                        continue
-                    stack.append((ni, nj))
-                    visited.add(stack[-1])
+        def dfs(i,j):
+            if grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            
+            dire = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for a, b in dire:
+                nx, ny = i + a, j + b
+                if 0 <= nx < m and 0 <= ny < n:
+                    dfs(nx, ny)
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and (i, j) not in visited:
-                    visit(i, j)
-                    islands += 1
-        
-        return islands
-
+                if grid[i][j] == "1":
+                    dfs(i,j)
+                    count += 1
+        return count

d46e16d1bd93c085b2c86adbfb64864aaa58adf9
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-16 20:23:04 +0000

    [LeetCode Sync] Runtime - 94 ms (21.16%), Memory - 21.5 MB (73.50%)

--- leetcode/0348-design-tic-tac-toe/solution.py
+++ leetcode/0348-design-tic-tac-toe/solution.py
@@ -1,43 +1,53 @@
-from collections import defaultdict
 class TicTacToe:
 
     def __init__(self, n: int):
         self.n = n
-        # player -> #row -> count
-        self.rows = { 1: defaultdict(int), 2: defaultdict(int) }
-        # player -> #col -> count
-        self.cols = { 1: defaultdict(int), 2: defaultdict(int) }
-        # player -> #diagonal (1 / 2) -> count
-        self.diagonals = { 1: defaultdict(int), 2: defaultdict(int) }
+        self.verticalMoves = {
+            1: defaultdict(int), 2: defaultdict(int)
+        }
+        self.horizontalMoves = {
+            1: defaultdict(int), 2: defaultdict(int)
+        }
+        self.diagonalMoves = {
+            1: defaultdict(int), 2: defaultdict(int)
+        }
+        """
+        verticalMoves: {
+            1: {0:2, 1:1, 2:1}
+            2: {0:1, 1:1, 2:1}
+        }
+        horizontalMoves: {
+            1: {0:1, 2:3}
+            2: {0:1, 1:2}
+        }
+        diagonalMoves: {
+            1: {0:1, 1:2}
+            2: {0:2, 1:1}
+        }
+        22 . 1
+        1. . .
+        01 . .
+         0 1 2
+        """
 
     def move(self, row: int, col: int, player: int) -> int:
-        """
-         0,1,2
-        0
-        1
-        2
-        """
-        self.rows[player][row] += 1
-        if self.rows[player][row] == self.n:
+        self.verticalMoves[player][col]+=1
+        if self.verticalMoves[player][col] == self.n:
             return player
-
-        self.cols[player][col] += 1
-        if self.cols[player][col] == self.n:
-            return player
-
-        if row == col:
-            self.diagonals[player][1] += 1
-            if self.diagonals[player][1] == self.n:
-                return player
-
-        if row == self.n - col - 1:
-            self.diagonals[player][2] += 1
-            if self.diagonals[player][2] == self.n:
-                return player
         
-        return 0
+        self.horizontalMoves[player][row] += 1
+        if self.horizontalMoves[player][row] == self.n:
+            return player
+        
+        if row == col and self.n % 2 == 1 and row == self.n // 2:
+            self.diagonalMoves[player][0] += 1
+            self.diagonalMoves[player][1] += 1
+        elif row == col:
+            self.diagonalMoves[player][1] += 1
+        elif row == self.n - col - 1:
+            self.diagonalMoves[player][0] += 1
 
-
+        return player if self.diagonalMoves[player][0] == self.n or self.diagonalMoves[player][1] == self.n else 0
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
 # param_1 = obj.move(row,col,player)

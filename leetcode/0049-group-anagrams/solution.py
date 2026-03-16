46ba5e5ca4cdf91a14b5d8ecd3ef9c522173aa0a
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-16 19:56:29 +0000

    [LeetCode Sync] Runtime - 7 ms (98.52%), Memory - 22 MB (86.67%)

--- leetcode/0049-group-anagrams/solution.py
+++ leetcode/0049-group-anagrams/solution.py
@@ -1,7 +1,18 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = defaultdict(list)
+        """
+        ["eat","tea","tan","ate","nat","bat"]
+
+        {
+            'aet': ['eat','tea', 'ate']
+            'ant': ['tan', 'nat']
+            'abt': ['bat']
+        }
+        """
+        groups = defaultdict(list)
+
         for s in strs:
-            group[''.join(sorted(s))].append(s)
-        return list(group.values())
+            groups[''.join(sorted(s))].append(s)
+        
+        return [ groups[key] for key in groups ]

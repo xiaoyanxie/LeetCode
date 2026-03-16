8e5dc3cea54a75fa8be1e0d78bc151c778b71f08
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-16 18:34:29 +0000

    [LeetCode Sync] Runtime - 39 ms (35.56%), Memory - 21.7 MB (94.20%)

--- leetcode/0560-subarray-sum-equals-k/solution.py
+++ leetcode/0560-subarray-sum-equals-k/solution.py
@@ -1,18 +1,42 @@
-from collections import defaultdict
+from collections import defaultdict 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         """
-        
+        1,2,3 k=3
+
+        prefixSumFreq = {
+            0: 1
+            3: 2
+            5: 1
+        }
+        total = 6
+      0 3 6  5  3 8  6
+        3,3,-1,-2,5,-2
+        3
+        3,3,-1,-2
+          3
+          3,-1,-2,5,-2
+               -2,5
+                  5,-2
+
+        total = 0
+        prefixSum = 0
+        for each num in nums:
+            prefixSum += num
+            if prefixSum - k in prefixFreq:
+                total += prefixFreq[prefixSum - k]
+            prefixFreq[prefixSum] += 1
         """
 
-        runningSum = 0
-        prefixSums = defaultdict(int)
-        prefixSums[0] = 1
-        cnt = 0
+        total = 0
+        prefixFreq = defaultdict(int)
+        prefixFreq[0] = 1
+        prefixSum = 0
+
         for num in nums:
-            runningSum += num
-            # runningSum - prefixSum = k
-            if (runningSum - k) in prefixSums:
-                cnt += prefixSums[runningSum - k]
-            prefixSums[runningSum] += 1
-        return cnt
+            prefixSum += num
+            if (prefixSum - k) in prefixFreq:
+                total += prefixFreq[prefixSum - k]
+            prefixFreq[prefixSum] += 1
+        
+        return total

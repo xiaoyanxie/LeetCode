ca26f1cd5a0d3783355b4176205aac991f65cd4c
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-18 05:18:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (34.10%)

--- leetcode/0394-decode-string/solution.py
+++ leetcode/0394-decode-string/solution.py
@@ -1,22 +1,59 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # 3[a2[c]]
-        stack = [] # [ ]
-        currNum = 0 # 
-        currStr = '' # accaccacc
-        for i, c in enumerate(s):
-            if c.isdigit():
-                currNum = currNum * 10 + int(c)
-            elif ord('a') <= ord(c) <= ord('z'):
-                currStr += c
-            elif c == '[':
-                stack.append((currStr, currNum))
-                currStr = ''
-                currNum = 0
-            elif c == ']':
-                prevStr, N = stack.pop() # '', 3
-                currStr = prevStr + N * currStr
-            else:
-                raise Exception('unreachable')
+        """
+        3[a]2[bc]
 
-        return currStr
+        3[a2[c2[b]2[b]]]
+                  c
+        k = 0
+        acc = [cbb]
+        stack: 3, a, 2, 
+
+        2[a2[c]bc]3[cd]ef
+         ^
+
+        k = 2
+
+        
+
+        [21 * accbc, ]
+         cdcdcdef
+
+        3[a2[c2[b]2[b]]]
+                       i
+        stack:  
+        acc: acbbbbcbbbb acbbbbcbbbb acbbbbcbbbb
+        k: 0
+
+        3[a]2[bc]
+           c
+        stack: aaa
+        acc: bcbc
+        k: 0
+
+        3[a2[c]]3[a]a
+                   ^
+
+        stack: ([accaccacc], 3)
+        k: 0
+        acc: [a]
+        """
+        stack = []
+        acc = []
+        k = 0
+        for c in s:
+            if c.isdigit():
+                k = k * 10 + int(c)
+            elif 'a' <= c <= 'z':
+                assert k == 0
+                acc.append(c)
+            elif c == '[':
+                assert k > 0
+                stack.append((acc, k))
+                k = 0
+                acc = []
+            elif c == ']':
+                prevAcc, prevK = stack.pop()
+                acc = prevAcc + prevK * acc
+    
+        return ''.join(acc)

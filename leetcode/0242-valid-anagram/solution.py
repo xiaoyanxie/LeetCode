d9abd013afc92d1ddb239530c128257a915c4b7f
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-19 06:23:25 +0000

    [LeetCode Sync] Runtime - 11 ms (76.01%), Memory - 19.5 MB (40.57%)

--- leetcode/0242-valid-anagram/solution.py
+++ leetcode/0242-valid-anagram/solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        
+        table = collections.defaultdict(int)
+        for ele in s:
+            table[ele] += 1
+        
+        for ele in t:
+            if ele not in table:
+                return False
+            elif table[ele] == 0:
+                return False
+            table[ele] -= 1
+        
+        for value in table.values():
+            if value != 0:
+                return False
+            
+        return True
+ 

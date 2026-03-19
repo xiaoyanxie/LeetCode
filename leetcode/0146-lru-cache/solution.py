fb38c815c9a2d81bd75a77061c88c05460f194e4
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-19 06:34:51 +0000

    [LeetCode Sync] Runtime - 116 ms (67.20%), Memory - 78.9 MB (5.31%)

--- leetcode/0146-lru-cache/solution.py
+++ leetcode/0146-lru-cache/solution.py
@@ -4,13 +4,14 @@ class Node:
         self.val = val
         self.prev = None
         self.next = None
-    
+
     def unlink(self):
         self.prev.next = self.next
         self.next.prev = self.prev
-        self.next = None
+
         self.prev = None
-    
+        self.next = None
+
     def insertLeft(self, node):
         self.prev.next = node
         node.prev = self.prev
@@ -18,53 +19,65 @@ class Node:
         self.prev = node
         node.next = self
 
+    def toStr(self) -> str:
+        s = ''
+        curr = self
+        while curr:
+            s += f'node({curr.key},{curr.val}) <-> '
+            curr = curr.next
+        return s
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        """
-        "LRUCache", "put", "put",   "get", "put",  "get", "put", "get", "get", "get"
-        [2],        [1, 1], [2, 2], [1],   [3, 3], [2],   [4, 4], [1],  [3],   [4]
-
-        size = 2
-        cache: {
-            1: node(1, 1),
-            2: node(2, 2)
-        }
-        head <-> node(1, 1) <-> tail
-        """
-        self.capacity = capacity
-        self.cache = {}
         self.head = Node()
         self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
+        self.cache = {}
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
+        # print(f'get({key})')
         if key not in self.cache:
             return -1
         
+        # update access
         node = self.cache[key]
         node.unlink()
         self.tail.insertLeft(node)
+        # print(self.head.toStr())
         return node.val
 
+    """
+    cap: 2
+    index: {
+        1 -> node(1,1),
+        2 -> node(2,2)
+    }
+    head <-> node(1,1) <-> node(2,2) <-> tail
+    """
     def put(self, key: int, value: int) -> None:
+        # print(f'put({key}, {value})')
         if key in self.cache:
             node = self.cache[key]
             node.val = value
             node.unlink()
             self.tail.insertLeft(node)
+            # print(self.head.toStr())
             return
-
+        
+        # ensure capacity
         if len(self.cache) == self.capacity:
-            # evcit
             evcit = self.head.next
             evcit.unlink()
             del self.cache[evcit.key]
-
+        
         node = Node(key, value)
         self.cache[key] = node
         self.tail.insertLeft(node)
+        # print(self.head.toStr())
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)

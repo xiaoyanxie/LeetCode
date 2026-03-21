10feed18647d40729c88e1462d12a625e1aca5f5
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-21 19:56:32 +0000

    [LeetCode Sync] Runtime - 484 ms (95.08%), Memory - 19.7 MB (22.07%)

--- leetcode/0079-word-search/solution.py
+++ leetcode/0079-word-search/solution.py
@@ -4,31 +4,43 @@ class Solution:
         ABCCED
              ^
         """
-        visited = set()
+        # visited = set()
         self.dire = [[1,0],[-1,0],[0,1],[0,-1]]
 
         def dfs(idx, i, j):
-            
             if board[i][j] != word[idx]:
                 return False
-            visited.add((i,j))
-            
-            if board[i][j] == word[idx] and idx == len(word) - 1:
+            if idx == len(word) - 1:
                 return True
-
+            
+            
+            # visited.add((i,j))
+            board[i][j] = "#"
+            
             for a,b in self.dire:
                 nx, ny = i + a, j + b
-                if 0 <= nx < m and 0 <= ny < n and (nx, ny) not in visited:
+                if 0 <= nx < m and 0 <= ny < n:
                     if dfs(idx + 1, nx, ny):
                         return True 
-            visited.remove((i,j))
+            board[i][j] = word[idx]
             return False
 
         m = len(board)
         n = len(board[0])
+        #pruning
+        table = collections.defaultdict(int)
         for i in range(m):
             for j in range(n):
-                if board[i][j] == word[0] and (i,j) not in visited:
+                table[board[i][j]] += 1
+
+        wordCount = collections.Counter(word)
+        for key in wordCount.keys():
+            if wordCount[key] > table[key]:
+                return False
+        #pruning ends
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
                     if dfs(0, i, j):
                         return True
         

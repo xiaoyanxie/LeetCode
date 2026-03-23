a0cdc9b55dc6b0c9dd3a295a8635aab7a8c78239
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-23 21:44:30 +0000

    [LeetCode Sync] Runtime - 933 ms (64.11%), Memory - 68.5 MB (53.36%)

--- leetcode/0211-design-add-and-search-words-data-structure/solution.py
+++ leetcode/0211-design-add-and-search-words-data-structure/solution.py
@@ -6,54 +6,52 @@ class TrieNode:
 class WordDictionary:
 
     def __init__(self):
-        self.trie = TrieNode("#")
+        self.trie = TrieNode()
 
     def addWord(self, word: str) -> None:
-        root = self.trie
+        node = self.trie
         for char in word:
-            if char not in root.children:
-                root.children[char] = TrieNode(char)
-            root = root.children[char]
-        root.is_last = True
-
+            if char not in node.children:
+                node.children[char] = TrieNode(char)
+            node = node.children[char]
+        node.is_last = True
+            
     def search(self, word: str) -> bool:
-        """
+        root = self.trie
+        def find(node, idx): #(g,3 )
+            if idx == len(word): # idx = 3
+                return node.is_last
+            
+            c = word[idx] # c = g
+            if c != ".":
+                if c in node.children:
+                    return find(node.children[c], idx + 1)
+                else:
+                    return False
+            
+            # c = a - z
+            for char in node.children: 
+                found = find(node.children[char], idx + 1)#b,c,d
+                if found:
+                    return True
+            return False
+
+        return find(root, 0)
+"""
         dummy
           |
           a
         | | |
-        c b d
+        b c d
         |   |
-        d   e
+        e   g
+word = a.g
+dfs(dummy, 0) ->dfs(node(a), 1) -> dfs(node(b), 2) -> False
+                                -> dfs(node(c), 2) -> False
+                                -> dfs(node(d), 2) -> dfs(node(g), 3) -> is_last -> True
 
-        query: a.e
+"""        
 
-        dfs(dummy, 0) -> dfs(node(a), 1) -> dfs(node(c), 2) -> False
-                                         -> dfs(node(b), 2) -> False
-                                         -> dfs(node(d), 2) -> dfs(node(e), 3) -> True
-        """
-        def dfs(root, idx): # node(e), 3
-            if idx == len(word):
-                return root.is_last
-
-            c = word[idx] # e
-
-            # 1. char is a-z
-            if c != '.':
-                if c not in root.children:
-                    return False
-                return dfs(root.children[c], idx + 1)
-            
-            # 2. char is .
-            for char in root.children: # c b [d]
-                nxt = root.children[char] # [d]
-                found = dfs(nxt, idx + 1)
-                if found:
-                    return True
-            return False
-        return dfs(self.trie, 0)
-
-    
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)

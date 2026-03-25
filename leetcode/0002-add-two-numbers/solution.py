e312f749a5ecafcfe3975d0d5f7930ab9393892b
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-25 18:49:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (89.51%)

--- leetcode/0002-add-two-numbers/solution.py
+++ leetcode/0002-add-two-numbers/solution.py
@@ -6,32 +6,30 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         """
-        head: () -> node(7) -> node(0)
-        curr: node(0)
-        carry = 1
-        [2,4,3]
-        [5,6,4]
-             ^
+        carry: 1
+        l1 = [2,4,5], l2 = [5,6,4]
+                  l1            l2
+
+        root -> node(7) -> node(0) -> node(0) -> node(1)
         """
         carry = 0
-        head = ListNode()
-        curr = head
+        root = ListNode()
+        curr = root
         while l1 or l2:
             n = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
             if n >= 10:
-                carry = 1
                 n -= 10
+                carry = 1
             else:
                 carry = 0
-            
             curr.next = ListNode(n)
             curr = curr.next
             if l1:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
-        
+
         if carry == 1:
             curr.next = ListNode(1)
-
-        return head.next
+        
+        return root.next

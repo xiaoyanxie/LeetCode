6eeb21bb12742ac1022d2ab7074a046be4b539c4
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-25 16:53:02 +0000

    [LeetCode Sync] Runtime - 1 ms (96.83%), Memory - 21.1 MB (68.22%)

--- leetcode/0042-trapping-rain-water/solution.py
+++ leetcode/0042-trapping-rain-water/solution.py
@@ -1,41 +1,41 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         """
-  total:16
-   lmax:7
-   rmax:7
-
-        5,5,1,7,1,1,5,2,7,6
-                  l
-                      r
-
-        4,2,0,3,2,5
-          l
-                  r
-
+        trapped = 0
+        l, r = 0, len(height)
+        loop:
+            if lmax <= rmax:
+                # move l
+                trapped += lmax - height[l]
+                # calculate lmax
+            else:
+                # move r
+                trapped += rmax - height[r]
+                # calculate rmax
+        
+        lmax = 3
+        rmax = 2
+        total: 6
         0,1,0,2,1,0,1,3,2,1,2,1
-                    l
-                          r
+                      l
+                      r
         """
         l, r = 0, len(height) - 1
-        lmax = height[l]
-        rmax = height[r]
+        lmax, rmax = height[l], height[r]
         total = 0
 
-        def collect(h):
-            delta = min(rmax, lmax) - h
-            return delta if delta > 0 else 0
-
-        while l <= r:
+        while l < r:
             if lmax <= rmax:
-                total += collect(height[l])
-                # print(f'{lmax} <= {rmax} collect {collect(height[l])} at {l}')
-                lmax = max(lmax, height[l])
                 l += 1
+                if lmax >= height[l]:
+                    total += lmax - height[l]
+                else:
+                    lmax = height[l]
             else:
-                total += collect(height[r])
-                # print(f'{lmax} > {rmax} collect {collect(height[r])} at {r}')
-                rmax = max(rmax, height[r])
                 r -= 1
+                if rmax >= height[r]:
+                    total += rmax - height[r] 
+                else:
+                    rmax = height[r]
         
         return total

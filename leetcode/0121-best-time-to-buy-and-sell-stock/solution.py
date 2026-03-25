e76a9f3fe42658112385fc16ec2ca48ef1630b91
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-25 18:42:34 +0000

    [LeetCode Sync] Runtime - 60 ms (31.67%), Memory - 28.6 MB (59.61%)

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.py
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
+        profit: 5
         buy = 1
-        profit = 5
         7,1,5,3,6,4
-        7 1 1 1 1 1
+                   i
         """
-        buy = prices[0]
-        maxProfit = 0
-        for p in prices:
-            if p < buy:
-                buy = p
-            else:
-                maxProfit = max(p - buy, maxProfit)
-        return maxProfit
+        profit = 0
+        buy = 10**5
+        for price in prices:
+            profit = max(profit, price - buy)
+            buy = min(buy, price)
+        return profit

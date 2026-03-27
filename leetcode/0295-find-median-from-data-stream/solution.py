1cd30f9abe97c589909e97083b26637c9fbd5b0e
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2026-03-27 06:18:44 +0000

    [LeetCode Sync] Runtime - 138 ms (91.23%), Memory - 41.9 MB (69.83%)

--- leetcode/0295-find-median-from-data-stream/solution.py
+++ leetcode/0295-find-median-from-data-stream/solution.py
@@ -1,24 +1,46 @@
-import heapq
 class MedianFinder:
 
     def __init__(self):
-        # 2,5,2,9,3,4,6,1
-        #       ^
-        # [2,2] [5,9]
-        self.small = []
-        self.large = []
+        """
+        1,2
+        idx = 2
+        stream[0] + stream
+        123if a number is smaller than minh[0], add to maxh
+        if a number is bigger than maxh[0],add to minh
+        minh=[2]
+        maxh=[1]
+        """
+
+        self.minh = []
+        self.maxh = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -num)
-        heapq.heappush(self.large, -heapq.heappop(self.small))
-        if len(self.large) - len(self.small) > 1:
-            heapq.heappush(self.small, -heapq.heappop(self.large))
+        if not self.minh and not self.maxh:
+            heapq.heappush(self.maxh, -num)
+            
+        elif num > (-self.maxh[0]):
+            heapq.heappush(self.minh, num)
 
-    def findMedian(self) -> float:
-        if len(self.large) == len(self.small):
-            return (-self.small[0] + self.large[0]) / 2.0
+            if len(self.minh) - len(self.maxh) > 1:
+                tmp = heapq.heappop(self.minh)
+                heapq.heappush(self.maxh, -tmp)
+               
         else:
-            return float(self.large[0])
+            heapq.heappush(self.maxh, -num)
+
+            if len(self.maxh) - len(self.minh) > 1:
+                tmp = heapq.heappop(self.maxh)
+                heapq.heappush(self.minh, -tmp)
+                
+    
+    def findMedian(self) -> float:
+        if len(self.minh) == len(self.maxh):
+            return (self.minh[0] - self.maxh[0]) / 2
+        elif len(self.minh) > len(self.maxh):
+        
+            return self.minh[0]
+        else:
+            return -self.maxh[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
